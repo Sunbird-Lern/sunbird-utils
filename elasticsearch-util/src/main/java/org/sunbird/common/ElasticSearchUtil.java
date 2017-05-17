31b13a504cdd2efa91e45861b21e61744bafbeed
manzarul.haque <manzarul.haque>
2017-05-17 19:03:45 +0530

    issue #1 chnages for ES util

--- elasticsearch-util/src/main/java/org/sunbird/common/ElasticSearchUtil.java
+++ elasticsearch-util/src/main/java/org/sunbird/common/ElasticSearchUtil.java
@@ -14,58 +14,62 @@ public class ElasticSearchUtil{
 	
 	/**
 	 * This method will put a new data entry inside Elastic search.
-	 * @param url String , url of running elastic search
+	 * @param ip  String, ES Ip
+	 * @param port String ES port
 	 * @param index String  ES index name
 	 * @param type  String  ES type name
 	 * @param data Map<String,Object> 
 	 */
-	public static void createData(String url, String index,String type, Map<String,Object> data) {
+	public static void createData(String ip,String port, String index,String type, Map<String,Object> data) {
 		
 	}
     
 	/**
-	 * This method will provide data form ES based on incoming key value pair.
-	 * @param url String, url of running ES
+	 * This method will provide data form ES based on incoming identifier.
+	 * @param ip  String, ES Ip
+	 * @param port String ES port
 	 * @param type String
-	 * @param data  Map<String,Object>
+	 * @param identifier String
 	 * @return Map<String,Object>
 	 */
-	public static Map<String,Object> getData(String url,String type,Map<String,Object> data ) {
+	public static Map<String,Object> getData(String ip,String port,String index,String type,String identifier) {
 		return null;
 	}
 	/**
 	 * This method will do the data search inside ES. based on incoming search data.
-	 * @param url String, url of running ES
+	 * @param ip  String, ES Ip
+	 * @param port String ES port
 	 * @param index String
 	 * @param type String
 	 * @param searchData  Map<String,Object>
 	 * @return Map<String,Object>
 	 */
-	public static Map<String,Object> searchData (String url,String index,String type, Map<String ,Object> searchData) {
-		//TODO need to discuss 
+	public static Map<String,Object> searchData (String ip,String port,String type, Map<String ,Object> searchData) {
 		return null;
 	}
     
 	/**
 	 * This method will update data based on identifier.take the data based on identifier and merge with 
 	 * incoming data then update it.
-	 * @param url  String, url of running ES
+	 * @param ip  String, ES Ip
+	 * @param port String ES port
 	 * @param index String 
 	 * @param type String
 	 * @param data Map<String,Object>
 	 */
-	public static void updateData (String url, String index,String type, Map<String,Object> data) {
+	public static void updateData (String ip,String port,String type, Map<String,Object> data) {
 		
 	}
 	
 	/**
 	 * This method will remove data from ES based on identifier.
-	 * @param url  String, url of running ES
+	 * @param ip  String, ES Ip
+	 * @param port String ES port
 	 * @param index String
 	 * @param type String
 	 * @param identifier String
 	 */
-	public static void removeData(String url, String index,String type,String identifier) {
+	public static void removeData(String ip,String port,String type,String identifier) {
 		
 	}
 }
--- elasticsearch-util/src/main/java/org/sunbird/helper/ElasticSearchQueryBuilder.java
+++ elasticsearch-util/src/main/java/org/sunbird/helper/ElasticSearchQueryBuilder.java
@@ -3,6 +3,13 @@
  */
 package org.sunbird.helper;
 
+import java.net.InetAddress;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
 /**
  * This class will create elasticsearch query
  * from requested search data. 
@@ -10,5 +17,10 @@ package org.sunbird.helper;
  *
  */
 public class ElasticSearchQueryBuilder {
-
+	Settings settings = Settings.builder()
+	        .put("cluster.name", "myClusterName").build();
+	
+	/*TransportClient client = new PreBuiltTransportClient(settings)
+	        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLocalHost(), 9300))
+	         .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLocalHost(), 9300));*/
 }
