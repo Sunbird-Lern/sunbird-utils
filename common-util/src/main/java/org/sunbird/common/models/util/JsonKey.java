5d30a6f03219d7c00caf29ef42409332970f6394
arvindyadav108 <arvind.glaitm108@gmail.com>
2017-05-23 14:19:42 +0530

    Issue #1 Developement: constants added in JsonKey .

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -58,4 +58,6 @@ public class JsonKey {
     public static final String LAST_READ_CONTENT_STATUS = "lastreadcontentstatus";
     public static final String LAST_READ_CONTENT_VERSION = "lastreadcontentversion";
     public static final String PUBLISH_COURSE = "publishcourse";
+    public static final String COURSE_CREATED_FOR = "createdfor";
+    public static final String ENROLLMENT_END_DATE = "enrollmentenddate";
 }
