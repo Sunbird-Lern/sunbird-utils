8856a7eeff1393368008e05d6ddec4577e6aa67a
manzarul.haque <manzarul.haque>
2017-05-25 11:12:20 +0530

    Issue #1 feat:adding cassandra table column map

---
+++ common-util/src/main/java/org/sunbird/common/models/util/PropertiesCache.java
@@ -0,0 +1,43 @@
+package org.sunbird.common.models.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/*
+ * @author Amit Kumar
+ * this class is used for reading cassndra table column name and will map
+ * it with java standard name.
+ */
+public class PropertiesCache {
+	private final String fileName = "cassandratablecolumn.properties";
+	private final Properties configProp = new Properties();
+
+	private PropertiesCache()
+	   {
+	      InputStream in = this.getClass().getClassLoader().getResourceAsStream(fileName);
+	      try {
+	          configProp.load(in);
+	      } catch (IOException e) {
+	          e.printStackTrace();
+	      }
+	   }
+	 
+	   private static class LazyHolder
+	   {
+	      private static final PropertiesCache INSTANCE = new PropertiesCache();
+	   }
+	 
+	   public static PropertiesCache getInstance()
+	   {
+	      return LazyHolder.INSTANCE;
+	   }
+	    
+	   public String getProperty(String key){
+	      return configProp.getProperty(key);
+	   }
+	    
+	   public static void main(String[] args) {
+		   PropertiesCache.getInstance().getProperty("es.host.name");
+	}
+}
