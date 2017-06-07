c2b936090db8b0857dd78fd81ffe86b320bba784
manzarul.haque <manzarul.haque>
2017-06-07 12:02:47 +0530

    issue feat:#171 feat:adding for section and page request validator

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -97,5 +97,10 @@ public class JsonKey {
     public static final String DB_USERNAME="db.username";
     public static final String DB_PASSWORD="db.password";
     public static final String DB_KEYSPACE="db.keyspace";
+	public static final String SECTION_NAME = "sectionName";
+	public static final String SEARCH_QUERY = "searchQuery";
+	public static final String SECTION_DATA_TYPE = "sectionDataType";
+	public static final String SECTION_DISPLAY = "display";
+	public static final String PAGE_NAME = "pageName";
 
 }
