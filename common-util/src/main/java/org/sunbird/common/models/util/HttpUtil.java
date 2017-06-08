2b7a8eed1e58dc9aa67a62c15763c7d2972bd421
manzarul.haque <manzarul.haque>
2017-06-08 12:05:06 +0530

    issue #183 feat:adding httputility class

--- common-util/src/main/java/org/sunbird/common/models/util/HttpUtil.java
+++ common-util/src/main/java/org/sunbird/common/models/util/HttpUtil.java
@@ -3,14 +3,20 @@
  */
 package org.sunbird.common.models.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import org.json.JSONObject;
 
 /**
  * This utility method will handle external 
@@ -20,6 +26,7 @@ import java.util.Map.Entry;
  */
 public class HttpUtil {
 	private static HttpURLConnection httpURLConnection;
+	private static final LogHelper LOGGER = LogHelper.getInstance(HttpUtil.class.getName());
 	/**
      * Makes an HTTP request using GET method to the specified URL.
      *
@@ -27,23 +34,23 @@ public class HttpUtil {
      *            the URL of the remote server
      * @param headers 
      *              the Map <String,String>           
-     * @return An HttpURLConnection object
+     * @return An String object
      * @throws IOException
      *             thrown if any I/O error occurred
      */
-    public static HttpURLConnection sendGetRequest(String requestURL,Map<String,String> headers)
-            throws IOException {
-        URL url = new URL(requestURL);
-        httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setUseCaches(false);
-        httpURLConnection.setDoInput(true); 
-        httpURLConnection.setDoOutput(false);
-        httpURLConnection.setRequestMethod(ProjectUtil.Method.GET.name());
-        if(headers != null && headers.size()>0) {
-        	setHeaders(httpURLConnection, headers);
-        }
-        return httpURLConnection;
-    }
+    public static String sendGetRequest(String requestURL,Map<String,String> headers)
+			throws IOException {
+		URL url = new URL(requestURL);
+		httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setUseCaches(false);
+		httpURLConnection.setDoInput(true);
+		httpURLConnection.setDoOutput(false);
+		httpURLConnection.setRequestMethod(ProjectUtil.Method.GET.name());
+		if (headers != null && headers.size() > 0) {
+			setHeaders(httpURLConnection, headers);
+		}
+		return getResponse(httpURLConnection);
+	}
      
     
     
@@ -59,7 +66,7 @@ public class HttpUtil {
      * @throws IOException
      *             thrown if any I/O error occurred
      */
-    public static HttpURLConnection sendPostRequest(String requestURL,
+    public static String sendPostRequest(String requestURL,
 			Map<String, String> params,Map<String,String> headers) throws IOException {
 		URL url = new URL(requestURL);
 		httpURLConnection = (HttpURLConnection) url.openConnection();
@@ -87,8 +94,70 @@ public class HttpUtil {
 		 writer.write(requestParams.toString());
 		 writer.flush();
 		 }
-		return httpURLConnection;
+		return getResponse(httpURLConnection);
 	}
+    
+    /**
+     * Makes an HTTP request using POST method to the specified URL.
+     *
+     * @param requestURL
+     *            the URL of the remote server
+     * @param params
+     *            A map containing POST data in form of key-value pairs
+     * @return An HttpURLConnection object
+     * @throws IOException
+     *             thrown if any I/O error occurred
+     */
+    public static String sendPostRequest(String requestURL,
+			String params,Map<String,String> headers) throws IOException {
+		URL url = new URL(requestURL);
+		httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setUseCaches(false);
+		httpURLConnection.setDoInput(true);
+        httpURLConnection.setRequestMethod(ProjectUtil.Method.POST.name());
+			httpURLConnection.setDoOutput(true); 
+		 if(headers !=null && headers.size()>0) {
+			 setHeaders(httpURLConnection, headers);
+		 }
+		 OutputStreamWriter writer = new OutputStreamWriter(httpURLConnection.getOutputStream());
+		 writer.write(params);
+		 writer.flush();
+		return getResponse(httpURLConnection);
+	}
+    
+	private static String getResponse(HttpURLConnection httpURLConnection) {
+		InputStream inStream = null;
+		BufferedReader reader = null;
+		StringBuilder builder = new StringBuilder();
+		try {
+			inStream = httpURLConnection.getInputStream();
+			reader = new BufferedReader(new InputStreamReader(inStream));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+				}
+			}
+			if (inStream != null) {
+				try {
+					inStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (httpURLConnection != null) {
+				httpURLConnection.disconnect();
+			}
+		}
+		return builder.toString();
+	} 
     
     /**
      * Set the header for request.
@@ -102,8 +171,5 @@ public class HttpUtil {
     	     httpURLConnection.setRequestProperty(entry.getKey(),entry.getValue());
     	 }
     }
-    
-    
-    
 
 }
