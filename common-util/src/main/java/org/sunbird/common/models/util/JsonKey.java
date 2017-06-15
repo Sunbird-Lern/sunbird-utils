1793b3fa35be72ef5c5c57ff3e2c7c49d97b8d1c
amit-tarento <amit.kumar@tarento.com>
2017-06-15 12:14:51 +0530

    Issue #217 feat:assesment api change

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -152,4 +152,7 @@ public class JsonKey {
 	public static final String REQUEST = "request";
 	public static final String SUNBIRD_ACTOR_FILE_PATH = "sunbird_actor_file_path";
 	public static final String USER_COUNT = "userCount";
+	
+	public static final String ASSESMENT_ITEM_DB = "assesment_item_db";
+    public static final String ASSESMENT_EVAL_DB = "assesment_eval_db";
 }
