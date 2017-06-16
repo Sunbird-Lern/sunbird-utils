632d4a8e66ea1faf1ba4f10ae80946391e667150
manzarul.haque <manzarul.haque>
2017-06-16 10:59:21 +0530

    issue #213 feat:changes for assessment evaluation

--- common-util/src/main/java/org/sunbird/common/services/AssessmentEvaluator.java
+++ common-util/src/main/java/org/sunbird/common/services/AssessmentEvaluator.java
@@ -19,8 +19,15 @@ public interface AssessmentEvaluator {
      * list will contains assessment details. Default implementation is
      * to collect the best among all.
      * @param data Map<String, List<Map<String, Object>>>
-     * @return Map<String, Map<String, Object>>
+     * @return Map<String, List<Map<String, Object>>>
      */
-	public Map<String, Map<String, Object>> evaluateAssessment(Map<String, List<Map<String, Object>>> data);
+	public Map<String, List<Map<String, Object>>>evaluateAssessment(Map<String, List<Map<String, Object>>> data);
+	/**
+	 * This  method will take user assessment item id as a key and value as score , max score and some other
+	 * details for that assessment item id. 
+	 * @param evaluatedData Map<String, List<Map<String, Object>>>
+	 * @return Map<String,Object> 
+	 */
+	public Map<String,Object> evaluateResult (Map<String, List<Map<String, Object>>> evaluatedData);
 
 }
