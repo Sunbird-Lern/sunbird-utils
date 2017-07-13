00a8f38c5d64b22c7ffb9ea1615ad360e5d0672d
vijethas <vijetha.nayak@riflexions.com>
2017-07-13 13:06:57 +0530

    Issue #348 feat:Implementation Logger as per the spec related changes

--- common-util/src/main/java/org/sunbird/common/models/util/LogHelper.java
+++ common-util/src/main/java/org/sunbird/common/models/util/LogHelper.java
@@ -1,7 +1,7 @@
 package org.sunbird.common.models.util;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.sunbird.common.request.ExecutionContext;
 
 /**
