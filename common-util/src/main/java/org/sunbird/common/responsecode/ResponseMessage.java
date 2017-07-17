80e8aa4f7eac6c2f8bd4a0405e2b6192aab004ac
arvindyadav108 <arvind.glaitm108@gmail.com>
2017-07-17 13:21:11 +0530

    Issue #364 Developement : Constant added .

--- common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
+++ common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
@@ -106,6 +106,8 @@ public interface ResponseMessage {
 		public static final String JOB_NAME_ERROR = "Job Name is required.";
 		public static final String NAME_OF_ORGANISATION_ERROR = "Organisation Name is required.";
 		public static final String ROLES_MISSING = "user role is required.";
+        public static final String INVALID_DATE_FORMAT = "Invalid Date format . Date format should be : yyyy-MM-dd hh:mm:ss:SSSZ";
+        public static final String SRC_EXTERNAL_ID_ALREADY_EXIST = "SOURCE WITH EXTERNAL ID ALREADY EXIST .";
 	}
     public interface Key {
         public static final String UNAUTHORISE_USER = "UNAUTHORISE_USER";
@@ -207,5 +209,7 @@ public interface ResponseMessage {
 		public static final String JOB_NAME_ERROR = "JOB_NAME_ERROR";
 		public static final String NAME_OF_ORGANISATION_ERROR = "NAME_OF_ORGANISATION_ERROR";
 		public static final String ROLES_MISSING = "ROLES_REQUIRED_ERROR";
+		public static final String INVALID_DATE_FORMAT = "INVALID_DATE_FORMAT";
+		public static final String SRC_EXTERNAL_ID_ALREADY_EXIST = "SRC_EXTERNAL_ID_ALREADY_EXIST";
     }
 }
