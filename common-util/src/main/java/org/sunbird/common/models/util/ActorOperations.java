d7d7e389ef44ad93e4a7aa1b8eaca507889a3058
amit-tarento <amit.kumar@tarento.com>
2017-08-03 16:47:58 +0530

    Issue #388 feat:bulk user api impl

--- common-util/src/main/java/org/sunbird/common/models/util/ActorOperations.java
+++ common-util/src/main/java/org/sunbird/common/models/util/ActorOperations.java
@@ -32,7 +32,8 @@ public enum ActorOperations {
     REJECT_USER_ORGANISATION("rejectUserOrganisation"),INSERT_ORG_INFO_ELASTIC("insertOrgInfoToElastic"),
     DOWNLOAD_USERS("downloadUsersData"),DOWNLOAD_ORGS("downlaodOrg"),BLOCK_USER("blockUser")
     ,DELETE_BY_IDENTIFIER("deleteByIdentifier"),
-    BULK_UPLOAD("bulkUpload"),PROCESS_BULK_UPLOAD("processBulkUpload"),ASSIGN_ROLES("assignRoles"), UNBLOCK_USER("unblockUser");
+    BULK_UPLOAD("bulkUpload"),PROCESS_BULK_UPLOAD("processBulkUpload"),ASSIGN_ROLES("assignRoles"), UNBLOCK_USER("unblockUser"),
+    GET_BULK_OP_STATUS("getBulkOpStatus");
     
     private String value;
 
