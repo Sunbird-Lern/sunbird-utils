ae9b9bfd5eae281c4ab5923c0fec524189d216d2
manzarul.haque <manzarul.haque>
2017-08-14 15:48:42 +0530

    issue #485 feat:adding new job to verify course published status

---
+++ cassandra-dac/src/main/java/org/sunbird/helper/ServiceFactory.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.sunbird.helper;
+
+import org.sunbird.cassandra.CassandraOperation;
+import org.sunbird.cassandraimpl.CassandraOperationImpl;
+
+/**
+ * This class will provide cassandraOperationImpl instance.
+ * @author Manzarul
+ */
+public class ServiceFactory {
+  
+  /**
+   * On call of this method , it will provide a new 
+   * CassandraOperationImpl instance on each call.
+   * @return
+   */
+  public static CassandraOperation getInstance() {
+    return new CassandraOperationImpl();
+  }
+
+}
