ec3bc0479691d34c841aec70461dfa1522b9815b
amit-tarento <amit.kumar@tarento.com>
2017-08-17 15:38:26 +0530

    Issue #443 feat:getBatchDetails

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -375,4 +375,8 @@ public class JsonKey {
   public static final String DATE_HISTOGRAM = "DATE_HISTOGRAM";
   public static final String EKSTEP_TAG_API_URL ="ekstep.tag.api.url";
   public static final String HASH_TAG_ID = "hashtagid";
+  public static final String COUNT_INCREMENT_DATE = "countIncrementDate";
+  public static final String COUNT_DECREMENT_DATE = "countDecrementDate";
+  public static final String EKSTEP_CONTENT_UPDATE_URL = "ekstep.content.update.url";
+  public static final String CONTENT_NAME = "contentName";
 }
