43da184390d55e3e11b8a06da0102c0a21638ac6
amit-tarento <amit.kumar@tarento.com>
2017-08-18 15:38:42 +0530

    Issue #443 feat:getBatchDetails

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -385,4 +385,7 @@ public class JsonKey {
   public static final String SUNBIRD_PG_USER = "sunbird_pg_user";
   public static final String SUNBIRD_PG_PASSWORD = "sunbird_pg_password";
   public static final String LOCATION = "location";
+  public static final String DOB = "dob";
+  public static final String GENDER = "gender";
+  public static final String THUMBNAIL = "thumbnail";
 }
