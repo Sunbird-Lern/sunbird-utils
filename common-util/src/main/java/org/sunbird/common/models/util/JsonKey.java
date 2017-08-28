a6961bb411682888fb274cf993d061a2cc07033e
manzarul.haque <manzarul.haque>
2017-08-28 12:38:30 +0530

    issue #504 feat: adding for sending emails

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -399,5 +399,9 @@ public class JsonKey {
   public static final String THEME = "theme";
   public static final String EKSTEP_BASE_URL = "ekstep_api_base_url";
   public static final String EKSTEP_COURSE_PUBLISH_URL ="ekstep_course_publish_url";
-
+  public static final String EMAIL_SERVER_HOST = "mail_server_host";
+  public static final String EMAIL_SERVER_PORT  = "mail_server_port";
+  public static final String EMAIL_SERVER_USERNAME  = "mail_server_username";
+  public static final String EMAIL_SERVER_PASSWORD  = "mail_server_password";
+  public static final String EMAIL_SERVER_FROM  = "mail_server_from_email";
 }
