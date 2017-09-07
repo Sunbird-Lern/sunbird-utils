a7650fb2ca7d5060d0f330ad60aa60a288b7652f
arvindyadav108 <arvind.glaitm108@gmail.com>
2017-09-07 18:04:15 +0530

    Issue #380 Developement : code merge .

--- common-util/src/main/java/org/sunbird/common/models/util/ActorOperations.java
+++ common-util/src/main/java/org/sunbird/common/models/util/ActorOperations.java
@@ -49,7 +49,8 @@ public enum ActorOperations {
     ORG_CREATION_METRICS_REPORT("orgCreationMetricsReport"),ORG_CONSUMPTION_METRICS_REPORT("orgConsumptionMetricsReport"),
     EMAIL_SERVICE("emailService"),FILE_STORAGE_SERVICE("fileStorageService"),GET_ALL_BADGE("getAllBadge"),ADD_USER_BADGE("addUserBadge"),
     ADD_USER_BADGE_BKG("addUserBadgebackground"),FILE_GENERATION_AND_UPLOAD("fileGenerationAndUpload"),HEALTH_CHECK("healthCheck"),
-   SEND_MAIL("sendMail"), PROCESS_DATA("processData"), ACTOR("actor"),CASSANDRA("cassandra"),ES("es"),EKSTEP("ekstep");
+   SEND_MAIL("sendMail"), PROCESS_DATA("processData"), ACTOR("actor"),CASSANDRA("cassandra"),ES("es"),EKSTEP("ekstep"),
+    COURSE_PROGRESS_METRICS_DATA("courseProgressMetricsData");
 
     private String value;
 
