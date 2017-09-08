b5058e646dc60701263ba9781752499f1281a04b
vijethas <vijetha.nayak@riflexions.com>
2017-09-08 12:33:45 +0530

    Issue #393 feat:Org-Admin consumption,Course dashboard api mockup

--- common-util/src/main/java/org/sunbird/common/models/util/FileUtil.java
+++ common-util/src/main/java/org/sunbird/common/models/util/FileUtil.java
@@ -4,21 +4,35 @@ import java.io.File;
 import java.util.List;
 
 public abstract class FileUtil {
-  
-  public abstract File writeToFile(String fileName,List<List<Object>> dataValues);
-  
+
+  public abstract File writeToFile(String fileName, List<List<Object>> dataValues);
+
   @SuppressWarnings("unchecked")
   protected static String getListValue(Object obj) {
     List<Object> data = (List<Object>) obj;
     if (!(data.isEmpty())) {
-    StringBuffer sb = new StringBuffer();
-    for (Object value : data) {
-      sb.append((String) value).append(",");
-    }
-    sb.deleteCharAt(sb.length() - 1);
-    return sb.toString();
+      StringBuffer sb = new StringBuffer();
+      for (Object value : data) {
+        sb.append((String) value).append(",");
+      }
+      sb.deleteCharAt(sb.length() - 1);
+      return sb.toString();
     }
     return "";
+  }
+
+  public static FileUtil getFileUtil(String format) {
+    if (ProjectUtil.isStringNullOREmpty(format)) {
+      format = format.toLowerCase();
+    }
+    switch (format) {
+      case "excel": {
+        return (new ExcelFileUtil());
+      }
+      default: {
+        return (new ExcelFileUtil());
+      }
+    }
   }
 
 }
