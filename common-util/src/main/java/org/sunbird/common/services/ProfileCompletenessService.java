ccfddd6e82f0b32d3e1375b74dff00e5caad4700
manzarul.haque <manzarul.haque>
2017-09-25 15:52:20 +0530

    issues #SB_29,SB_32 feat:adding new feature for profile completeness

---
+++ common-util/src/main/java/org/sunbird/common/services/ProfileCompletenessService.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.sunbird.common.services;
+
+import java.util.Map;
+
+/**
+ * This interface will have method to 
+ * compute the profile completeness.
+ * @author Manzarul
+ *
+ */
+public interface ProfileCompletenessService {
+  
+  /**
+   * This method will compute the user profile completeness
+   * percentage based on attribute weighted settings. it will 
+   * provide completeness percentage value and list of all
+   * missing keys.
+   * @param profileData Map<String,Object>
+   * @return Map<String,Object>
+   */
+  public Map<String,Object> computeProfile (Map<String,Object> profileData);
+  
+}
