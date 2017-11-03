3a45a3a700b80e580c23eda7ebdee80527839268
AMIT KUMAR <amit.kumar@tarento.com>
2017-11-03 16:20:52 +0530

    Issue#538 feat:allowing user to update locId and hashtagId

--- common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
+++ common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
@@ -195,6 +195,8 @@ public interface ResponseMessage {
       public static final String INVALID_CLIENT_ID = "Please provide valid client id";
       public static final String USER_PHONE_UPDATE_FAILED = "user phone update is failed.";
       public static final String ES_UPDATE_FAILED = "Data insertion to ES failed.";
+      public static final String INVALID_LOCATION_ID = "Please provide valid location id.";
+      public static final String INVALID_HASHTAG_ID = "Please provide different hashTagId.This HashTagId is associated with some other organization.";
     }
     
     public interface Key {
@@ -384,5 +386,7 @@ public interface ResponseMessage {
         public static final String INVALID_CLIENT_ID = "INVALID_CLIENT_ID";
         public static final String USER_PHONE_UPDATE_FAILED = "USER_PHONE_UPDATE_FAILED";
         public static final String ES_UPDATE_FAILED = "ES_UPDATE_FAILED";
+        public static final String INVALID_LOCATION_ID = "INVALID_LOCATION_ID";
+        public static final String INVALID_HASHTAG_ID = "INVALID_HASHTAG_ID";
     }
 }
