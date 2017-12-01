49a3fabb047f355462644074cecca181ff0435a3
manzarul.haque <manzarul.haque>
2017-12-01 17:06:06 +0530

    Issues #SB-1025 feat: new maven project for sending SMS using msg91 gateway

---
+++ sunbird-notification/src/main/java/org/sunbird/notification/utils/PropertiesCache.java
@@ -0,0 +1,38 @@
+package org.sunbird.notification.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.jboss.logging.Logger;
+
+
+public class PropertiesCache {
+  private static Logger logger = Logger.getLogger(PropertiesCache.class);
+  private final String fileName = "configuration.properties";
+  private final Properties configProp = new Properties();
+
+  /**
+   * private default constructor
+   */
+  private PropertiesCache() {
+      InputStream in = this.getClass().getClassLoader().getResourceAsStream(fileName);
+      try {
+        configProp.load(in);
+      } catch (IOException e) {
+        logger.error("Error in properties cache", e);
+      }
+  }
+
+  private static class LazyHolder {
+    private static final PropertiesCache INSTANCE = new PropertiesCache();
+  }
+
+  public static PropertiesCache getInstance() {
+    return LazyHolder.INSTANCE;
+  }
+
+  public String getProperty(String key) {
+    return configProp.getProperty(key) != null ? configProp.getProperty(key) : null;
+  }
+}
