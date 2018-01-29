255174ec4b626627c4150e21b793674f242585f5
arvindyadav108 <arvind.glaitm108@gmail.com>
2018-01-29 15:32:12 +0530

    Issue # SB-1 feat : code merge .

---
+++ common-util/src/main/java/org/sunbird/telemetry/util/lmaxdisruptor/TelemetryEvents.java
@@ -0,0 +1,20 @@
+package org.sunbird.telemetry.util.lmaxdisruptor;
+
+/**
+ * Created by arvind on 29/1/18.
+ */
+public enum TelemetryEvents {
+
+  AUDIT("AUDIT"),SEARCH("SEARCH"),LOG("LOG"),ERROR("ERROR");
+
+  private String name;
+
+  TelemetryEvents(String name){
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}
