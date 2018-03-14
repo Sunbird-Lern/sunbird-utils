b095cf56b13992012de8068878bfe1ac868353cd
manzarul <manzarul07@gmail.com>
2018-03-14 15:31:20 +0530

    Issues #SB-2075 feat;adding a new key

--- common-util/src/main/java/org/sunbird/common/models/util/BadgingJsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/BadgingJsonKey.java
@@ -60,6 +60,7 @@ public class BadgingJsonKey {
   public static final String  BADGE_ASSERTION = "badgeAssertion";
   public static final String SLUG = "slug";
   public static final String VALID_BADGE_SUBTYPES = "sunbird_valid_badge_subtypes";
+  public static final String BADGE_CLASS_ID = "badgeId";
 
   private BadgingJsonKey(){}
 }
