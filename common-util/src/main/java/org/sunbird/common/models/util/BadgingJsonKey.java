72df93a6557ffc576c421b7130669e0cba2d611a
AMIT KUMAR <amit.kumar@tarento.com>
2018-03-21 18:20:39 +0530

    Issue #SB-1305 feat:code clean up

--- common-util/src/main/java/org/sunbird/common/models/util/BadgingJsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/BadgingJsonKey.java
@@ -55,8 +55,8 @@ public class BadgingJsonKey {
     public static final String VERIFY = "verify";
     public static final String REVOKED = "revoked";
     public static final String CREATED_TS = "createdTS";
-    public static final String BADGE_CLASS_NANE = "badgeClassName";
-    public static final String BADGE_CLASS_IMAGE = "badgeClassImage";
+    public static final String BADGE_CLASS_NANE = "name";
+    public static final String BADGE_CLASS_IMAGE = "image";
     public static final String BADGE_ASSERTION = "badgeAssertion";
     public static final String SLUG = "slug";
     public static final String VALID_BADGE_SUBTYPES = "sunbird_valid_badge_subtypes";
@@ -64,7 +64,7 @@ public class BadgingJsonKey {
     public static final String USER_BADGE_ASSERTION_DB = "user_badge_assertion";
     public static final String BADGE_ASSERTIONS = "badgeAssertions";
     // this email will be set while sunbird installation and used to send the email
-    //in case of assertion , if user email is absent. 
+    // in case of assertion , if user email is absent.
     public static final String SUNBIRD_INSTALLATION_EMAIL = "sunbird_installation_email";
 
     private BadgingJsonKey() {}
