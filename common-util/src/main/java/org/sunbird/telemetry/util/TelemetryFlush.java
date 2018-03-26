0571cbbf99629d79ee516583d42c5076a6e425ee
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-03-26 17:22:20 +0530

    Issue #SB-00 fix: changes to logging.

--- common-util/src/main/java/org/sunbird/telemetry/util/TelemetryFlush.java
+++ common-util/src/main/java/org/sunbird/telemetry/util/TelemetryFlush.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.commons.lang.StringUtils;
 import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.models.util.PropertiesCache;
 
@@ -21,7 +22,8 @@ public class TelemetryFlush {
 
 	private static TelemetryFlush telemetryFlush;
 
-	private List<TelemetryDispatcher> telemetryDispatcher = TelemetryDispatcherFactory.getInstanceList(JsonKey.SUNBIRD_LMS_TELEMETRY, JsonKey.EK_STEP);
+	private List<TelemetryDispatcher> telemetryDispatcher = TelemetryDispatcherFactory
+			.getInstanceList(JsonKey.EK_STEP);
 
 	public static TelemetryFlush getInstance() {
 		if (telemetryFlush == null) {
@@ -35,18 +37,24 @@ public class TelemetryFlush {
 	}
 
 	public TelemetryFlush() {
- 	String queueThreshold = PropertiesCache.getInstance().getProperty(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
-		if (!StringUtils.isBlank(queueThreshold)
-				&& !queueThreshold.equalsIgnoreCase(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE)) {
-			try {
-				this.thresholdSize = Integer.parseInt(queueThreshold.trim());
-			} catch (Exception ex) {
-				ProjectLogger.log("Threshold size from config is not integer", ex);
-			}
-		}
+//		String queueThreshold = PropertiesCache.getInstance().getProperty(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
+//		if (!StringUtils.isBlank(queueThreshold)
+//				&& !queueThreshold.equalsIgnoreCase(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE)) {
+//			try {
+//				this.thresholdSize = Integer.parseInt(queueThreshold.trim());
+//			} catch (Exception ex) {
+//				ProjectLogger.log("Threshold size from config is not integer", ex);
+//			}
+//		}
+		this.thresholdSize = 1;
 	}
 
 	public void flushTelemetry(String message) {
+		writeToQueue(message);
+	}
+	
+	public void flushTelemetry(String message, boolean audit) {
+		ProjectLogger.log("Audit event pushed to queue.", LoggerEnum.INFO.name());
 		writeToQueue(message);
 	}
 
@@ -54,8 +62,9 @@ public class TelemetryFlush {
 		queue.offer(message);
 		if (queue.size() >= thresholdSize) {
 			List<String> list = new ArrayList<>();
-			for (int i = 1; i <= thresholdSize; i++) {
-				String obj = queue.poll();
+			int size = queue.size();
+			for (int i = 1; i <= size; i++) {
+				String obj = queue.poll(); 
 				if (obj == null) {
 					break;
 				} else {
@@ -65,7 +74,7 @@ public class TelemetryFlush {
 			for (TelemetryDispatcher dispatch : telemetryDispatcher) {
 				if (dispatch != null) {
 					dispatch.dispatchTelemetryEvent(list);
-				}else {
+				} else {
 					ProjectLogger.log("TelemetryDispatcher instance is coming as null " + dispatch);
 				}
 			}
