0b1c00030924b322e4b6072b0f5758ae0e12715c
AMIT KUMAR <amit.kumar@tarento.com>
2018-03-28 16:49:54 +0530

    Issue #SB-0 feat:gzip support changes

--- common-util/src/main/java/org/sunbird/telemetry/util/TelemetryFlush.java
+++ common-util/src/main/java/org/sunbird/telemetry/util/TelemetryFlush.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-
 import org.apache.commons.lang.StringUtils;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.models.util.PropertiesCache;
 
@@ -16,63 +14,64 @@ import org.sunbird.common.models.util.PropertiesCache;
  */
 public class TelemetryFlush {
 
-	private Queue<String> queue = new ConcurrentLinkedQueue<>();
+    private Queue<String> queue = new ConcurrentLinkedQueue<>();
 
-	private int thresholdSize = 20;
+    private int thresholdSize = 20;
 
-	private static TelemetryFlush telemetryFlush;
+    private static TelemetryFlush telemetryFlush;
 
-	private List<TelemetryDispatcher> telemetryDispatcher = TelemetryDispatcherFactory
-			.getInstanceList(JsonKey.EK_STEP);
+    private List<TelemetryDispatcher> telemetryDispatcher = TelemetryDispatcherFactory
+            .getInstanceList(JsonKey.EK_STEP, JsonKey.SUNBIRD_LMS_TELEMETRY);
 
-	public static TelemetryFlush getInstance() {
-		if (telemetryFlush == null) {
-			synchronized (TelemetryFlush.class) {
-				if (telemetryFlush == null) {
-					telemetryFlush = new TelemetryFlush();
-				}
-			}
-		}
-		return telemetryFlush;
-	}
+    public static TelemetryFlush getInstance() {
+        if (telemetryFlush == null) {
+            synchronized (TelemetryFlush.class) {
+                if (telemetryFlush == null) {
+                    telemetryFlush = new TelemetryFlush();
+                }
+            }
+        }
+        return telemetryFlush;
+    }
 
-	public TelemetryFlush() {
-		String queueThreshold = PropertiesCache.getInstance().getProperty(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
-		if (!StringUtils.isBlank(queueThreshold)
-				&& !queueThreshold.equalsIgnoreCase(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE)) {
-			try {
-				this.thresholdSize = Integer.parseInt(queueThreshold.trim());
-			} catch (Exception ex) {
-				ProjectLogger.log("Threshold size from config is not integer", ex);
-			}
-		}
-	}
+    public TelemetryFlush() {
+        String queueThreshold =
+                PropertiesCache.getInstance().getProperty(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
+        if (!StringUtils.isBlank(queueThreshold)
+                && !queueThreshold.equalsIgnoreCase(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE)) {
+            try {
+                this.thresholdSize = Integer.parseInt(queueThreshold.trim());
+            } catch (Exception ex) {
+                ProjectLogger.log("Threshold size from config is not integer", ex);
+            }
+        }
+    }
 
-	public void flushTelemetry(String message) {
-		writeToQueue(message);
-	}
+    public void flushTelemetry(String message) {
+        writeToQueue(message);
+    }
 
-	private void writeToQueue(String message) {
-		queue.offer(message);
-		if (queue.size() >= thresholdSize) {
-			List<String> list = new ArrayList<>();
-			for (int i = 1; i <= thresholdSize; i++) {
-				String obj = queue.poll(); 
-				if (obj == null) {
-					break;
-				} else {
-					list.add(obj);
-				}
-			}
-			for (TelemetryDispatcher dispatch : telemetryDispatcher) {
-				if (dispatch != null) {
-					dispatch.dispatchTelemetryEvent(list);
-				} else {
-					ProjectLogger.log("TelemetryDispatcher instance is coming as null " + dispatch);
-				}
-			}
-		}
+    private void writeToQueue(String message) {
+        queue.offer(message);
+        if (queue.size() >= thresholdSize) {
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i <= thresholdSize; i++) {
+                String obj = queue.poll();
+                if (obj == null) {
+                    break;
+                } else {
+                    list.add(obj);
+                }
+            }
+            for (TelemetryDispatcher dispatch : telemetryDispatcher) {
+                if (dispatch != null) {
+                    dispatch.dispatchTelemetryEvent(list);
+                } else {
+                    ProjectLogger.log("TelemetryDispatcher instance is coming as null " + dispatch);
+                }
+            }
+        }
 
-	}
+    }
 
 }
