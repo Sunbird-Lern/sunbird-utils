00b6d828cd09dad0b05b7689cbe78df8576d4b7c
AMIT KUMAR <amit.kumar@tarento.com>
2018-04-25 16:54:09 +0530

    Issue # SB-2684 feat:added location support for org api

--- common-util/src/main/java/org/sunbird/common/models/util/GeoLocationJsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/GeoLocationJsonKey.java
@@ -7,7 +7,7 @@ public class GeoLocationJsonKey {
 
   public static final String PARENT_CODE = "parentCode";
   public static final String CODE = "code";
-  public static final String LOCATION_TYPE = "locationType";
+  public static final String LOCATION_TYPE = "type";
   public static final String PARENT_ID = "parentId";
   public static final String SUNBIRD_VALID_LOCATION_TYPES = "sunbird_valid_location_types";
   public static final String PROPERTY_NAME = "name";
--- common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
+++ common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
@@ -283,7 +283,8 @@ public interface ResponseMessage {
     String INVALID_LOCN_ID = "Please provide valid locationId.";
     String UNUPDATABLE_FIELD = "Can not update field - {0}";
     String MANDATORY_HEADER_MISSING = "Mandatory header {0} is missing.";
-    String INVALID_PARAMETER_VALUE = "Invalid {0} for {1}.Please provide valid values.";
+    String INVALID_PARAMETER_VALUE =
+        "Invalid value {0} for parameter {1}. Please provide a valid value.";
   }
 
   interface Key {
