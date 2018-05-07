8e054007190aee5705d200bb933b8f3a341d9a03
AMIT KUMAR <amit.kumar@tarento.com>
2018-05-07 16:51:26 +0530

    Issue #SB-3063 feat: code changes as per review comments

--- common-util/src/test/java/org/sunbird/common/validator/location/BaseLocationRequestValidatorTest.java
+++ common-util/src/test/java/org/sunbird/common/validator/location/BaseLocationRequestValidatorTest.java
@@ -1,117 +1,32 @@
-/*package org.sunbird.common.validator.location;
+package org.sunbird.common.validator.location;
 
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.sunbird.actorutil.InterServiceCommunication;
-import org.sunbird.actorutil.InterServiceCommunicationFactory;
-import org.sunbird.actorutil.impl.InterServiceCommunicationImpl;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.util.GeoLocationJsonKey;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.LocationActorOperation;
 import org.sunbird.common.request.Request;
 
-*//**
+/**
  * Test case for Base Location request validator.
  *
  * @author arvind on 30/4/18.
- *//*
+ */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({
-  InterServiceCommunicationFactory.class,
-  InterServiceCommunication.class,
-  InterServiceCommunicationImpl.class,
-  BaseLocationRequestValidator.class
-})
-@PowerMockIgnore({"javax.management.*", "javax.net.ssl.*", "javax.security.*"})
 public class BaseLocationRequestValidatorTest {
 
   private String LOCATION_NAME = "location-name";
   private String LOCATION_CODE = "location_code";
   private String LOC_TYPE_STATE = "STATE";
   static BaseLocationRequestValidator validator = null;
-
-  private static InterServiceCommunication interServiceCommunication = null;
-
-  private static Map<String, Object> block = new HashMap<>();
-  private static Map<String, Object> district = new HashMap<>();
-  private static Map<String, Object> state = new HashMap<>();
-  private static Request request = new Request();
-  private static List<Map<String, Object>> locationList = new ArrayList<>();
-  private static List<String> codeList = new ArrayList<>();
-  private static List<String> locationIdsList = new ArrayList<>();
-
-  @BeforeClass
-  public static void setUp() {
-    block.put(JsonKey.TYPE, "block");
-    block.put(JsonKey.NAME, "block_name");
-    block.put(JsonKey.CODE, "blk1");
-    block.put(GeoLocationJsonKey.PARENT_CODE, "dis1");
-
-    district.put(JsonKey.TYPE, "district");
-    district.put(JsonKey.NAME, "district_name");
-    district.put(JsonKey.CODE, "dis1");
-    district.put(GeoLocationJsonKey.PARENT_CODE, "state1");
-
-    state.put(JsonKey.TYPE, "state");
-    state.put(JsonKey.NAME, "state_name");
-    state.put(JsonKey.CODE, "state1");
-
-    locationList.add(block);
-    locationList.add(district);
-    locationList.add(state);
-
-    codeList.add("blk1");
-
-    locationIdsList.addAll(Arrays.asList("blk1", "dis1", "state1"));
-
-    Map<String, Object> filters = new HashMap<>();
-    filters.put(GeoLocationJsonKey.CODE, codeList);
-    Map<String, Object> locMap = new HashMap<>();
-    locMap.put(JsonKey.FILTERS, filters);
-    request.setOperation(LocationActorOperation.SEARCH_LOCATION.getValue());
-    request.getRequest().putAll(locMap);
-
-    PowerMockito.mockStatic(InterServiceCommunicationFactory.class);
-    interServiceCommunication = PowerMockito.mock(InterServiceCommunication.class);
-    PowerMockito.when(InterServiceCommunicationFactory.getInstance())
-        .thenReturn(interServiceCommunication);
-    PowerMockito.when(interServiceCommunication.getResponse(Mockito.any(), Mockito.any()))
-        .thenReturn(locationList);
-
-    validator = PowerMockito.mock(BaseLocationRequestValidator.class);
-    PowerMockito.when(validator.getValidatedLocationIds(Mockito.anyList(), Mockito.any()))
-        .thenReturn(locationIdsList);
-  }
-
-  @Test
-  public void testValidateLocationCode() {
-    List<String> list = null;
-    list = validator.getValidatedLocationIds(codeList, null);
-    Assert.assertNotNull(list);
-  }
-
-  @Test(expected = ProjectCommonException.class)
-  public void testValidateLocationCodeFail() {
-    validator.getValidatedLocationIds(null, null);
-  }
 
   @Test
   public void testValidateCreateLocationWithProperData() {
@@ -235,4 +150,3 @@ public class BaseLocationRequestValidatorTest {
     validator.validateSearchLocationRequest(request);
   }
 }
-*/
