b21ad80b00b26a392d1042e13c8bc0c587bb46e4
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-05-10 10:48:37 +0530

    Issue #000 fix: handle AskTimeout Exception in RequestRouter.

--- actor-core/src/main/java/org/sunbird/actor/router/RequestRouter.java
+++ actor-core/src/main/java/org/sunbird/actor/router/RequestRouter.java
@@ -100,7 +100,7 @@ public class RequestRouter extends BaseRouter {
 						parent.tell(failure, self());
 					} else if (failure instanceof akka.pattern.AskTimeoutException) {
 						ProjectCommonException exception = new ProjectCommonException(
-								ResponseCode.internalError.getErrorCode(), ResponseCode.internalError.getErrorMessage(),
+								ResponseCode.operationTimeout.getErrorCode(), ResponseCode.operationTimeout.getErrorMessage(),
 								ResponseCode.SERVER_ERROR.getResponseCode());
 						parent.tell(exception, self());
 						
