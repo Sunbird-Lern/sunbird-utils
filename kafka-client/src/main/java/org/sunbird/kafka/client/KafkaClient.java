ac6029f316cbd8f06105576a2f3f4a70165278e6
Manzarul <john@sample.com>
2018-05-18 11:06:22 +0530

    Issues #SB-2660 feat: code review rework

--- kafka-client/src/main/java/org/sunbird/kafka/client/KafkaClient.java
+++ kafka-client/src/main/java/org/sunbird/kafka/client/KafkaClient.java
@@ -1,81 +1,69 @@
 package org.sunbird.kafka.client;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.serialization.LongDeserializer;
-import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 
-/** @author Mahesh Kumar Gangula */
+/**
+ * @author Mahesh Kumar Gangula This class will create Kafka consumer and producer object. and
+ *     provide to caller. For each call it will create new consumer and producer object.
+ */
 public class KafkaClient {
 
-  private static String BOOTSTRAP_SERVERS = System.getenv("sunbird_telemetry_kafka_servers_config");
-  private static String topic = System.getenv("sunbird_telemetry_kafka_topic");
-  private static Producer<Long, String> producer;
-  private static Consumer<Long, String> consumer;
-
-  static {
-    if (StringUtils.isNotBlank(BOOTSTRAP_SERVERS) && StringUtils.isNotBlank(topic)) {
-      createProducer();
-      createConsumer();
-      listTopics();
-    } else {
-      ProjectLogger.log(
-          "either BOOTSTRAP_SERVERS or topic is empty=="
-              + StringUtils.isNotBlank(BOOTSTRAP_SERVERS)
-              + "  "
-              + StringUtils.isNotBlank(topic),
-          LoggerEnum.INFO.name());
-    }
+  private static Producer<Long, String> createProducer(String bootstrapServers, String clientId) {
+    return new KafkaProducer<Long, String>(createPropertiesObj(bootstrapServers, clientId));
   }
 
-  private static void createProducer() {
-    ProjectLogger.log("Kafka server config: " + BOOTSTRAP_SERVERS, LoggerEnum.INFO.name());
-    ProjectLogger.log("Kafka topic name: " + topic, LoggerEnum.INFO.name());
-    Properties props = new Properties();
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-    props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaClientProducer");
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    producer = new KafkaProducer<Long, String>(props);
+  private static Consumer<Long, String> createConsumer(String bootstrapServers, String clientId) {
+    return new KafkaConsumer<>(createPropertiesObj(bootstrapServers, clientId));
   }
 
-  private static void createConsumer() {
+  /**
+   * his method will create a producer object and return to caller.
+   *
+   * @param bootstrapServers String EX:localhost:9092,localhost:9093,localhost:9094
+   * @param clientId String unique string value
+   * @return Producer<Long, String>
+   */
+  public static Producer<Long, String> initProducer(String bootstrapServers, String clientId) {
+    return createProducer(bootstrapServers, clientId);
+  }
+
+  /**
+   * This method will create a consumer object and return to caller.
+   *
+   * @param bootstrapServers String EX:localhost:9092,localhost:9093,localhost:9094
+   * @param clientId String unique string value
+   * @return Consumer<Long, String>
+   */
+  public static Consumer<Long, String> initConsumer(String bootstrapServers, String clientId) {
+    return createConsumer(bootstrapServers, clientId);
+  }
+
+  /**
+   * This method will generate properties object based on passed bootstrapServer and clientId
+   * details.
+   *
+   * @param bootstrapServers String EX:localhost:9092,localhost:9093,localhost:9094
+   * @param clientId String unique string value
+   * @return Properties
+   */
+  private static Properties createPropertiesObj(String bootstrapServers, String clientId) {
+    ProjectLogger.log(
+        "Kafka server config: and topic name " + bootstrapServers + " Topic--" + clientId,
+        LoggerEnum.INFO.name());
     Properties props = new Properties();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-    props.put(ConsumerConfig.CLIENT_ID_CONFIG, "KafkaClientConsumer");
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-    consumer = new KafkaConsumer<>(props);
-  }
-
-  public static Producer<Long, String> getProducer() {
-    return producer;
-  }
-
-  protected static Consumer<Long, String> getConsumer() {
-    return consumer;
-  }
-
-  public static void listTopics() {
-    Consumer<Long, String> consumer = getConsumer();
-    Map<String, List<PartitionInfo>> topics = consumer.listTopics();
-    ProjectLogger.log("Topics list from kafka: " + topics.keySet(), LoggerEnum.INFO.name());
-  }
-
-  public static String getTopic() {
-    return topic;
+    return props;
   }
 }
