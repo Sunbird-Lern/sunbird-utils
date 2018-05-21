7215cc73bdf90fcd7928468e1eb75708bfec1125
Vinaya Kumar B <vinayab@ilimi.in>
2018-05-21 14:27:45 +0530

    Issue #SB-766 fix: Fix formatting

--- kafka-client/src/main/java/org/sunbird/kafka/client/KafkaClient.java
+++ kafka-client/src/main/java/org/sunbird/kafka/client/KafkaClient.java
@@ -21,10 +21,9 @@ public class KafkaClient {
   /**
    * Creates a Kafka producer.
    *
-   * @param bootstrapServers Comma-separated list of host and port pairs that are the addresses
-   *                         of the Kafka brokers in a "bootstrap" Kafka cluster that a Kafka
-   *                         client connects to initially to bootstrap itself.
-   *                         e.g. localhost:9092,localhost:9093,localhost:9094
+   * @param bootstrapServers Comma-separated list of host and port pairs that are the addresses of
+   *     the Kafka brokers in a "bootstrap" Kafka cluster that a Kafka client connects to initially
+   *     to bootstrap itself. e.g. localhost:9092,localhost:9093,localhost:9094
    * @param clientId Identifier for Kafka producer
    * @return A Kafka producer for given configuration.
    */
@@ -35,10 +34,9 @@ public class KafkaClient {
   /**
    * Creates a Kafka consumer.
    *
-   * @param bootstrapServers Comma-separated list of host and port pairs that are the addresses
-   *                         of the Kafka brokers in a "bootstrap" Kafka cluster that a Kafka
-   *                         client connects to initially to bootstrap itself.
-   *                         e.g. localhost:9092,localhost:9093,localhost:9094
+   * @param bootstrapServers Comma-separated list of host and port pairs that are the addresses of
+   *     the Kafka brokers in a "bootstrap" Kafka cluster that a Kafka client connects to initially
+   *     to bootstrap itself. e.g. localhost:9092,localhost:9093,localhost:9094
    * @param clientId Identifier for Kafka consumer
    * @return A Kafka consumer for given configuration.
    */
@@ -49,15 +47,19 @@ public class KafkaClient {
   /**
    * Creates a properties instance required for creating a Kafka producer or consumer.
    *
-   * @param bootstrapServers Comma-separated list of host and port pairs that are the addresses
-   *                         of the Kafka brokers in a "bootstrap" Kafka cluster that a Kafka
-   *                         client connects to initially to bootstrap itself.
-   *                         e.g. localhost:9092,localhost:9093,localhost:9094
+   * @param bootstrapServers Comma-separated list of host and port pairs that are the addresses of
+   *     the Kafka brokers in a "bootstrap" Kafka cluster that a Kafka client connects to initially
+   *     to bootstrap itself. e.g. localhost:9092,localhost:9093,localhost:9094
    * @param clientId Identifier for Kafka producer or consumer
    * @return Properties required for instantiating a Kafka producer or consumer.
    */
   private static Properties createProperties(String bootstrapServers, String clientId) {
-    ProjectLogger.log("KafkaClient: createProperties called with bootstrapServers = " + bootstrapServers + " clientId = " + clientId, LoggerEnum.INFO.name());
+    ProjectLogger.log(
+        "KafkaClient: createProperties called with bootstrapServers = "
+            + bootstrapServers
+            + " clientId = "
+            + clientId,
+        LoggerEnum.INFO.name());
     Properties props = new Properties();
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
