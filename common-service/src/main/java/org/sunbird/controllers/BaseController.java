3226abd69cf62abc33199f2e3e9fb266ace6eb60
Manzarul <manzarul07@gmail.com>
2018-05-23 14:03:07 +0530

    Issues #SB-2660 fix: changes for scoping,from default to private

--- common-service/src/main/java/org/sunbird/controllers/BaseController.java
+++ common-service/src/main/java/org/sunbird/controllers/BaseController.java
@@ -37,7 +37,7 @@ public class BaseController extends Controller {
   private static final int AKKA_WAIT_TIME = 10;
   protected Timeout timeout = new Timeout(AKKA_WAIT_TIME, TimeUnit.SECONDS);
   private static Object actorRef = null;
-  static ResponseIdUtil util = new ResponseIdUtil();
+  private static ResponseIdUtil util = new ResponseIdUtil();
 
   static {
     try {
