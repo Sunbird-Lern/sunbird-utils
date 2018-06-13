f01f1ffd0ee8617223337b1f7ba0273bc0b33e83
AMIT KUMAR <amit.kumar@tarento.com>
2018-06-13 15:28:01 +0530

    Issue # SB-4229 feat: user microservice api added pre create method in user extension.

--- extensions/src/main/java/org/sunbird/extension/user/UserExtension.java
+++ extensions/src/main/java/org/sunbird/extension/user/UserExtension.java
@@ -3,16 +3,24 @@ package org.sunbird.extension.user;
 import java.util.Map;
 
 /**
- * UserExtension is the interface for all extensions of Sunbird user profile. It defines methods for performing CRUD operations on user profile.
- * @author Jaikumar Soundara Rajan
+ * UserExtension is the interface for all extensions of Sunbird user profile. It defines methods for
+ * performing CRUD operations on user profile.
  *
+ * @author Jaikumar Soundara Rajan
  */
 public interface UserExtension {
-	
-	/**
-	 * Creates user profile information.
-	 * @param userProfileMap User profile information
-	 */
-	public void create(Map<String,Object> userProfileMap);
-	
+
+  /**
+   * This method will be called before create method.
+   *
+   * @param userProfileMap User profile information
+   */
+  public void preCreate(Map<String, Object> userProfileMap);
+
+  /**
+   * Creates user profile information.
+   *
+   * @param userProfileMap User profile information
+   */
+  public void create(Map<String, Object> userProfileMap);
 }
