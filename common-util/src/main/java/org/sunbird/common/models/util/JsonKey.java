5db6be1175490869fbaa9a6ee983e6e1c0a63ee8
Manzarul <manzarul07@gmail.com>
2018-06-26 17:41:43 +0530

    Issues #SB-2928 fix: changes for email fixes and user bulk upload size

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -651,7 +651,8 @@ public final class JsonKey {
   public static final String ORG_PROVIDER = "orgProvider";
   public static final String SUNBIRD_TELEMETRY_BASE_URL = "sunbird_telemetry_base_url";
   public static final String DEFAULT_FRAMEWORK = "defaultFramework";
-  public static final String SUNBIRD_EMAIL_ENV = "sunbird_email_env";
+  public static final String SUNBIRD_INSTALLATION_DISPLAY_NAME =
+      "sunbird_installation_display_name";
 
   private JsonKey() {}
 }
