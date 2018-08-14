22c7a30c559bd620b94d4a5cb1d7867e4936d8ce
Rajat <rajat@mantralabsglobal.com>
2018-08-14 15:06:36 +0530

    changes in modek

--- models/src/main/java/org/sunbird/models/user/skill/Skill.java
+++ models/src/main/java/org/sunbird/models/user/skill/Skill.java
@@ -1,8 +1,12 @@
 package org.sunbird.models.user.skill;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.HashMap;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Skill {
   private String skillName;
   private String createdOn;
