9c61d2af4f52c6e4e4610cb8fa3ff5c212d771d4
Rajat <rajat@mantralabsglobal.com>
2018-08-14 12:25:47 +0530

    update skill model

--- models/src/main/java/org/sunbird/models/user/skill/Skill.java
+++ models/src/main/java/org/sunbird/models/user/skill/Skill.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 public class Skill {
   private String skillName;
-  private String addedAt;
+  private String createdOn;
   private List<HashMap<String, String>> endorsersList = null;
-  private String addedBy;
+  private String createdBy;
   private Integer endorsementCount;
   private String id;
   private String skillNameToLowercase;
   private String userId;
+  private String lastUpdatedBy;
+  private String lastUpdatedOn;
 
   public String getSkillName() {
     return skillName;
@@ -21,36 +23,12 @@ public class Skill {
     this.skillName = skillName;
   }
 
-  public String getAddedAt() {
-    return addedAt;
-  }
-
-  public void setAddedAt(String addedAt) {
-    this.addedAt = addedAt;
-  }
-
   public List<HashMap<String, String>> getEndorsersList() {
     return endorsersList;
   }
 
   public void setEndorsersList(List<HashMap<String, String>> endorsersList) {
     this.endorsersList = endorsersList;
-  }
-
-  public String getAddedBy() {
-    return addedBy;
-  }
-
-  public void setAddedBy(String addedBy) {
-    this.addedBy = addedBy;
-  }
-
-  public Integer getEndorsementcount() {
-    return endorsementCount;
-  }
-
-  public void setEndorsementcount(Integer endorsementCount) {
-    this.endorsementCount = endorsementCount;
   }
 
   public String getId() {
@@ -75,5 +53,45 @@ public class Skill {
 
   public void setUserId(String userId) {
     this.userId = userId;
+  }
+
+  public String getCreatedOn() {
+    return createdOn;
+  }
+
+  public void setCreatedOn(String createdOn) {
+    this.createdOn = createdOn;
+  }
+
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public Integer getEndorsementCount() {
+    return endorsementCount;
+  }
+
+  public void setEndorsementCount(Integer endorsementCount) {
+    this.endorsementCount = endorsementCount;
+  }
+
+  public String getLastUpdatedBy() {
+    return lastUpdatedBy;
+  }
+
+  public void setLastUpdatedBy(String lastUpdatedBy) {
+    this.lastUpdatedBy = lastUpdatedBy;
+  }
+
+  public String getLastUpdatedOn() {
+    return lastUpdatedOn;
+  }
+
+  public void setLastUpdatedOn(String lastUpdatedOn) {
+    this.lastUpdatedOn = lastUpdatedOn;
   }
 }
