303fb225aff070f25d9632124c42585326ca19ae
Rajat Gupta <rajatgupta@Rajats-MacBook-Pro.local>
2018-08-28 18:38:20 +0530

    review work

--- request-validator/src/main/java/org/sunbird/validator/systemsettings/SystemSettingsRequestValidator.java
+++ request-validator/src/main/java/org/sunbird/validator/systemsettings/SystemSettingsRequestValidator.java
@@ -5,19 +5,18 @@ import org.sunbird.common.request.BaseRequestValidator;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
-
 public class SystemSettingsRequestValidator extends BaseRequestValidator {
 
-    /**
-     * This methods validates the provided request for mandatory fields and throws exception if it
-     * fails
-     *
-     * @param request instance of Request class has the values to be updated to System Settings
-     *     (id,field,value)
-     */
-    public void validateUpdateSystemSetting(Request request) {
-        validateParam((String) request.getRequest().get(JsonKey.ID),ResponseCode.mandatoryParamsMissing);
-        validateParam((String) request.getRequest().get(JsonKey.FIELD),ResponseCode.mandatoryParamsMissing);
-        validateParam((String) request.getRequest().get(JsonKey.VALUE),ResponseCode.mandatoryParamsMissing);
-    }
+  /**
+   * @param request instance of Request class has the values to be updated to System Settings
+   *     (id,field,value)
+   */
+  public void validateSetSystemSetting(Request request) {
+    validateParam(
+        (String) request.getRequest().get(JsonKey.ID), ResponseCode.mandatoryParamsMissing);
+    validateParam(
+        (String) request.getRequest().get(JsonKey.FIELD), ResponseCode.mandatoryParamsMissing);
+    validateParam(
+        (String) request.getRequest().get(JsonKey.VALUE), ResponseCode.mandatoryParamsMissing);
+  }
 }
