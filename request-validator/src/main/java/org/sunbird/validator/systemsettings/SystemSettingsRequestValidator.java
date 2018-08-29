15077f9e880ed3fd7b0f50e7350a4cc4ef66a26c
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-08-29 19:07:45 +0530

    Update SystemSettingsRequestValidator.java

--- request-validator/src/main/java/org/sunbird/validator/systemsettings/SystemSettingsRequestValidator.java
+++ request-validator/src/main/java/org/sunbird/validator/systemsettings/SystemSettingsRequestValidator.java
@@ -6,11 +6,6 @@ import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
 public class SystemSettingsRequestValidator extends BaseRequestValidator {
-
-  /**
-   * @param request instance of Request class has the values to be updated to System Settings
-   *     (id,field,value)
-   */
   public void validateSetSystemSetting(Request request) {
     validateParam(
         (String) request.getRequest().get(JsonKey.ID), ResponseCode.mandatoryParamsMissing);
