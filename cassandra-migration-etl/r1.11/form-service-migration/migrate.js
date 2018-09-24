a7e398bd595a6eda63f0f7a0915b6cb7d047818e
Anoop HM <anoophm44@gmail.com>
2018-09-24 14:00:04 +0530

    Issue #SB-5038 feat: form plugin and ext frame work update

--- cassandra-migration-etl/r1.11/form-service-migration/migrate.js
+++ cassandra-migration-etl/r1.11/form-service-migration/migrate.js
@@ -5,15 +5,15 @@
 // 3. password: password for DB // optional
 // example: node migration.js 127.0.0.1:9200 username password
 
-const cassandra = require('cassandra-driver');
-let cassandra_client_options = { contactPoints: [process.argv[2]] };
+const cassandra = require("cassandra-driver");
+let cassandraClientOptions = { contactPoints: [process.argv[2]] };
 if(process.argv[3] && process.argv[4]){
-  cassandra_client_options.authProvider = new cassandra.auth.PlainTextAuthProvider(process.argv[3], process.argv[4]);
+  cassandraClientOptions.authProvider = new cassandra.auth.PlainTextAuthProvider(process.argv[3], process.argv[4]);
 }
-console.log('connecting to DB with', process.argv[2], process.argv[3], process.argv[4]);
-const client = new cassandra.Client(cassandra_client_options);
+console.log("connecting to DB with", process.argv[2], process.argv[3], process.argv[4]);
+const client = new cassandra.Client(cassandraClientOptions);
 
-let transformed_data = [];
+let transformedData = [];
 let dest_obj = {
   root_org: undefined,
   framework: undefined,
@@ -26,10 +26,10 @@ let dest_obj = {
   data: undefined
 }
 
-const query = 'SELECT * FROM sunbird.tenant_preference';
+const query = "SELECT * FROM sunbird.tenant_preference";
 client.execute(query)
   .then(result => {
-    console.log('no. of records to migrate:', result.rows.length);
+    console.log("no. of records to migrate:", result.rows.length);
     result.rows.forEach((row) => {
       let temp = Object.assign({}, dest_obj);
       temp.root_org = row.orgid;
@@ -42,16 +42,16 @@ client.execute(query)
         temp.subtype = "*";
         temp.action = "*";
       }
-      temp.component = "*"; //default, since it's new field
+      temp.component = "*"; //default, since it"s new field
       if (row.data) {
         if (typeof row.data === "string")
           try {
             row.data = JSON.parse(row.data);
           } catch (e) {
-            console.log('JSON parse error! :');
-            console.log('row.key', row.key);
-            console.log('row.orgid', row.orgid);
-            console.log('row.data', row.data);
+            console.log("JSON parse error! :");
+            console.log("row.key", row.key);
+            console.log("row.orgid", row.orgid);
+            console.log("row.data", row.data);
           }
         if (typeof row.data === "object") {
           Object.keys(row.data).forEach((key) => {
@@ -61,19 +61,19 @@ client.execute(query)
               temp.framework = key;
             }
             temp.data = row.data[key];
-            transformed_data.push(temp);
+            transformedData.push(temp);
           })
         } else {
           temp.data = row.data;
           temp.framework = "*";
-          transformed_data.push(temp);
+          transformedData.push(temp);
         }
       }
     })
   })
   .then(async _ => {
     // create keyspace if not exist
-    await client.execute('CREATE KEYSPACE IF NOT EXISTS qmzbm_form_service WITH REPLICATION = { \'class\' : \'SimpleStrategy\', \'replication_factor\' : 1 }')
+    await client.execute(`CREATE KEYSPACE IF NOT EXISTS qmzbm_form_service WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }`)
     // create table if not exist
     await client.execute(`CREATE TABLE IF NOT EXISTS qmzbm_form_service.form_data (
       root_org text,
@@ -87,11 +87,11 @@ client.execute(query)
       last_modified_on timestamp,
       PRIMARY KEY ((root_org, framework, type, subtype, action, component)))   
     `);
-    for(const data of transformed_data) {
+    for(const data of transformedData) {
       let query = "INSERT INTO qmzbm_form_service.form_data(root_org, framework, type, subtype, action, component, created_on, last_modified_on, data) values(?,?,?,?,?,?,?,?,?)"
       await client.execute(query, [data.root_org, data.framework, data.type, data.subtype, data.action, data.component, data.created_on, data.last_modified_on, JSON.stringify(data.data)], { prepare: true });
     }
-    console.log('no. of records migrated after denorm:', transformed_data.length);
+    console.log("no. of records migrated after denorm:", transformedData.length);
     process.exit(1);
   })
   .catch(error => {
@@ -99,7 +99,7 @@ client.execute(query)
     process.exit(1);
   })
 
-process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+process.on("unhandledRejection", (reason, p) => {
+  console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
   // application specific logging, throwing an error, or other logic here
 });
