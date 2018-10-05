009266190b65145e614dd601e8ce58cb04f913e2
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-10-05 13:17:52 +0530

    Update Message91GetSMSTest.java

--- sunbird-notification/src/test/java/org/sunbird/notification/sms/Message91GetSMSTest.java
+++ sunbird-notification/src/test/java/org/sunbird/notification/sms/Message91GetSMSTest.java
@@ -4,26 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.sunbird.notification.sms.providerimpl.Msg91SmsProvider;
 
-/** @author Manzarul */
-
 public class Message91GetSMSTest extends BaseMessageTest {
 
 	@Test
-	public void testSmsGetMethodSuccess() {
+	public void testSendSmsGetMethodSuccess() {
 		Msg91SmsProvider megObj = new Msg91SmsProvider();
 		boolean response = megObj.sendSmsGetMethod("4321111111", "say hai!");
 		Assert.assertTrue(response);
 	}
 
 	@Test
-	public void testSmsGetMethodWithoutMessage() {
+	public void testSendSmsGetMethodFailureWithoutMessage() {
 		Msg91SmsProvider megObj = new Msg91SmsProvider();
 		boolean response = megObj.sendSmsGetMethod("4321111111", "");
 		Assert.assertFalse(response);
 	}
 
 	@Test
-	public void testSmsGetMethodWithEmptySpace() {
+	public void testSendSmsGetMethodFailureWithEmptySpace() {
 		Msg91SmsProvider megObj = new Msg91SmsProvider();
 		boolean response = megObj.sendSmsGetMethod("4321111111", "  ");
 		Assert.assertFalse(response);
