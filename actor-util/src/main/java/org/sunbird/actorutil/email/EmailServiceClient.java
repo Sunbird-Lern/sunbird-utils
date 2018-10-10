d48416c2feb500070442419a189f9b1d68a0ee6a
Mayank Morya <moryamayank9@gmail.com>
2018-10-10 17:53:38 +0530

    email service client added

--- actor-util/src/main/java/org/sunbird/actorutil/email/EmailServiceClient.java
+++ actor-util/src/main/java/org/sunbird/actorutil/email/EmailServiceClient.java
@@ -9,8 +9,8 @@ public interface EmailServiceClient {
    * Send mail user from course.
    *
    * @param actorRef Actor reference
-   * @param request Request containing unenroll information
-   * @return Response containing unenroll request status
+   * @param request Request containing email realted information
+   * @return Response containing email send status
    */
   Response sendMail(ActorRef actorRef, Map<String, Object> request);
 }
