3b0fb5017c0a3de61b546f1134f293bfb350a7f8
rajat <rajatgupta0889@gmail.com>
2018-10-17 16:10:58 +0530

    Issue #SB-4221 fix: change method return type

--- actor-util/src/main/java/org/sunbird/actorutil/org/impl/OrganisationClientImpl.java
+++ actor-util/src/main/java/org/sunbird/actorutil/org/impl/OrganisationClientImpl.java
@@ -1,6 +1,8 @@
 package org.sunbird.actorutil.org.impl;
 
 import akka.actor.ActorRef;
+import java.util.HashMap;
+import java.util.Map;
 import org.sunbird.actorutil.InterServiceCommunication;
 import org.sunbird.actorutil.InterServiceCommunicationFactory;
 import org.sunbird.actorutil.org.OrganisationClient;
@@ -10,59 +12,56 @@ import org.sunbird.common.models.util.*;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by rajatgupta on 11/10/18.
- */
+/** Created by rajatgupta on 11/10/18. */
 public class OrganisationClientImpl implements OrganisationClient {
 
-    private static InterServiceCommunication interServiceCommunication =
-            InterServiceCommunicationFactory.getInstance();
+  private static InterServiceCommunication interServiceCommunication =
+      InterServiceCommunicationFactory.getInstance();
 
-    @Override
-    public Response createOrg(ActorRef actorRef, Map<String, Object> orgMap) {
-        Request request = new Request();
-        Map<String, Object> requestMap =new HashMap<>();
-        requestMap.put(JsonKey.ORGANISATION,orgMap);
-        request.setRequest(requestMap);
-        request.setOperation(ActorOperations.CREATE_ORG.getValue());
-        ProjectLogger.log("LocationClientImpl : callCreateOrganisation ", LoggerEnum.INFO);
-        Object obj = interServiceCommunication.getResponse(actorRef, request);
-        checkLocationResponseForException(obj);
-        if (obj instanceof Response) {
-            Response response = (Response) obj;
-            return response;
-        }
-        return null;
+  @Override
+  public String createOrg(ActorRef actorRef, Map<String, Object> orgMap) {
+    Request request = new Request();
+    Map<String, Object> requestMap = new HashMap<>();
+    requestMap.put(JsonKey.ORGANISATION, orgMap);
+    request.setRequest(requestMap);
+    request.setOperation(ActorOperations.CREATE_ORG.getValue());
+    ProjectLogger.log("LocationClientImpl : callCreateOrganisation ", LoggerEnum.INFO);
+    Object obj = interServiceCommunication.getResponse(actorRef, request);
+    checOrganisationResponseForException(obj);
+    String orgId = null;
+    if (obj instanceof Response) {
+      Response response = (Response) obj;
+      orgId = (String) response.get(JsonKey.ORGANISATION_ID);
     }
+    return orgId;
+  }
 
-    @Override
-    public Response updateOrg(ActorRef actorRef, Map<String, Object> orgMap) {
-        Request request = new Request();
-        Map<String, Object> requestMap =new HashMap<>();
-        requestMap.put(JsonKey.ORGANISATION,orgMap);
-        request.setRequest(requestMap);
-        request.setOperation(ActorOperations.UPDATE_ORG.getValue());
-        ProjectLogger.log("LocationClientImpl : callCreateOrganisation ", LoggerEnum.INFO);
-        Object obj = interServiceCommunication.getResponse(actorRef, request);
-        checkLocationResponseForException(obj);
-        if (obj instanceof Response) {
-            Response response = (Response) obj;
-            return response;
-        }
-        return null;
+  @Override
+  public String updateOrg(ActorRef actorRef, Map<String, Object> orgMap) {
+    Request request = new Request();
+    Map<String, Object> requestMap = new HashMap<>();
+    requestMap.put(JsonKey.ORGANISATION, orgMap);
+    request.setRequest(requestMap);
+    request.setOperation(ActorOperations.UPDATE_ORG.getValue());
+    ProjectLogger.log("LocationClientImpl : callCreateOrganisation ", LoggerEnum.INFO);
+    Object obj = interServiceCommunication.getResponse(actorRef, request);
+    checOrganisationResponseForException(obj);
+    String orgId = null;
+    if (obj instanceof Response) {
+      Response response = (Response) obj;
+      orgId = (String) response.get(JsonKey.ORGANISATION_ID);
     }
+    return orgId;
+  }
 
-    private void checkLocationResponseForException(Object obj) {
-        if (obj instanceof ProjectCommonException) {
-            throw (ProjectCommonException) obj;
-        } else if (obj instanceof Exception) {
-            throw new ProjectCommonException(
-                    ResponseCode.SERVER_ERROR.getErrorCode(),
-                    ResponseCode.SERVER_ERROR.getErrorMessage(),
-                    ResponseCode.SERVER_ERROR.getResponseCode());
-        }
+  private void checOrganisationResponseForException(Object obj) {
+    if (obj instanceof ProjectCommonException) {
+      throw (ProjectCommonException) obj;
+    } else if (obj instanceof Exception) {
+      throw new ProjectCommonException(
+          ResponseCode.SERVER_ERROR.getErrorCode(),
+          ResponseCode.SERVER_ERROR.getErrorMessage(),
+          ResponseCode.SERVER_ERROR.getResponseCode());
     }
+  }
 }
