c2d7ec571adc8cfdcb799e585dfb67325ad0908e
AMIT KUMAR <amit.kumar@tarento.com>
2018-11-23 13:15:28 +0530

    Update JsonKey.java

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -747,7 +747,6 @@ public final class JsonKey {
   public static final String CALLER_ID = "callerId";
   public static final String USER_TYPE = "userType";
   public static final String SELF_SIGN_UP = "SELF_SIGN_UP";
-  public static final String INSTALLATION_ACCOUNT_NAME = "accountName";
 
   private JsonKey() {}
 }
