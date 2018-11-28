87b97cbf7db8fb92d70fdc77aa7db3a2e67888ed
Kumar Gauraw <gaurawk@ilimi.in>
2018-11-28 14:48:57 +0530

    Issue #SB-9208 feat: added textbook constant

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -750,6 +750,7 @@ public final class JsonKey {
   public static final String USER_TYPE = "userType";
   public static final String SELF_SIGN_UP = "SELF_SIGN_UP";
   public static final String TEXTBOOK = "textbook";
+  public static final String TEXTBOOK_ID = "textbookId";
 
   public static final String COURSE_BATCH_URL = "courseBatchUrl";
   public static final String SUNBIRD_COURSE_BATCH_NOTIFICATION_SIGNATURE =
