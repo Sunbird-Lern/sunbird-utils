73a6ab84f3181ecc6be35f03a6b34c59c207725c
AMIT KUMAR <amit.kumar@tarento.com>
2018-12-13 15:08:38 +0530

    Issue #SC-682 feat:google sign in support

--- cassandra-dac/src/main/java/org/sunbird/cassandraimpl/CassandraOperationImpl.java
+++ cassandra-dac/src/main/java/org/sunbird/cassandraimpl/CassandraOperationImpl.java
@@ -221,7 +221,7 @@ public class CassandraOperationImpl implements CassandraOperation {
     try {
       Builder selectBuilder;
       if (CollectionUtils.isNotEmpty(fields)) {
-        selectBuilder = QueryBuilder.select((String[]) fields.toArray());
+        selectBuilder = QueryBuilder.select(fields.toArray(new String[fields.size()]));
       } else {
         selectBuilder = QueryBuilder.select().all();
       }
