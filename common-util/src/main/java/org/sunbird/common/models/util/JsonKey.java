4530255b63e67c919986dd631998bac57941f8b6
Mayank Morya <moryamayank9@gmail.com>
2018-12-14 19:33:38 +0530

    updated mandatory field check for framework id

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -813,7 +813,6 @@ public final class JsonKey {
   public static final String TNC_CONFIG = "tncConfig";
   public static final String TNC = "tnc";
   public static final String ACCEPT = "accept";
-  public static final String STRING = "String";
 
   private JsonKey() {}
 }
