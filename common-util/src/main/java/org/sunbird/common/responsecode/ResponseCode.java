1717af4f978a31a5a2f519211829706b917b8fa2
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2018-12-17 15:31:37 +0530

    Otp actor

--- common-util/src/main/java/org/sunbird/common/responsecode/ResponseCode.java
+++ common-util/src/main/java/org/sunbird/common/responsecode/ResponseCode.java
@@ -107,6 +107,10 @@ public enum ResponseCode {
   usrValidationError(
       ResponseMessage.Key.USR_DATA_VALIDATION_ERROR,
       ResponseMessage.Message.USR_DATA_VALIDATION_ERROR),
+  errorOtpNotFound(
+      ResponseMessage.Key.ERROR_OTP_NOT_FOUND, ResponseMessage.Message.ERROR_OTP_NOT_FOUND),
+  errorOtpMismatch(
+      ResponseMessage.Key.ERROR_OTP_MISMATCH, ResponseMessage.Message.ERROR_OTP_MISMATCH),
   enrollmentStartDateRequiredError(
       ResponseMessage.Key.ENROLLMENT_START_DATE_MISSING,
       ResponseMessage.Message.ENROLLMENT_START_DATE_MISSING),
