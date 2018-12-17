414646e531445edb1de09c33ac0be0f4496d2f64
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-12-17 16:24:11 +0530

    Update ResponseMessage.java

--- common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
+++ common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
@@ -61,8 +61,7 @@ public interface ResponseMessage {
     String ENROLLMENT_START_DATE_MISSING = "Enrollment start date is mandatory.";
     String COURSE_DURATION_MISSING = "Course duration is mandatory.";
     String LOGIN_TYPE_MISSING = "Login type is required.";
-    String ERROR_OTP_MISMATCH = "Invalid otp provided.";
-    String ERROR_OTP_NOT_FOUND = "Otp has expired or not generated.";
+    String ERROR_INVALID_OTP = "Invalid OTP.";
     String EMAIL_IN_USE = "Email already exists.";
     String USERNAME_EMAIL_IN_USE =
         "Username or Email is already in use. Please try with a different Username or Email.";
@@ -741,8 +740,7 @@ public interface ResponseMessage {
     String CSV_ROWS_EXCEEDS = "CSV_ROWS_EXCEEDS";
     String INVALID_TEXTBOOK_NAME = "INVALID_TEXTBOOK_NAME";
     String DUPLICATE_ROWS = "DUPLICATE_ROWS";
-    String ERROR_OTP_MISMATCH = "ERROR_OTP_MISMATCH";
-    String ERROR_OTP_NOT_FOUND = "ERROR_OTP_NOT_FOUND";
+    String ERROR_INVALID_OTP = "ERROR_INVALID_OTP";
     String REQUIRED_HEADER_MISSING = "REQUIRED_HEADER_MISSING";
     String REQUIRED_FIELD_MISSING = "REQUIRED_FIELD_MISSING";
     String BLANK_CSV_DATA = "BLANK_CSV_DATA";
