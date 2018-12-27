d6bcf995b72ab74d9729888594b8febf2887c5bf
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-12-27 17:18:59 +0530

    Issue #SB-7976 fix: Textbook read handling response

--- content-util/src/main/java/org/sunbird/content/util/TextBookTocUtil.java
+++ content-util/src/main/java/org/sunbird/content/util/TextBookTocUtil.java
@@ -1,22 +1,8 @@
 package org.sunbird.content.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.lang3.StringUtils;
-import org.sunbird.common.exception.ProjectCommonException;
-import org.sunbird.common.models.response.Response;
-import org.sunbird.common.responsecode.ResponseCode;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import static java.util.Objects.isNull;
 import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.sunbird.common.exception.ProjectCommonException.throwServerErrorException;
-import static org.sunbird.common.models.util.HttpUtil.sendGetRequest;
 import static org.sunbird.common.models.util.JsonKey.BEARER;
 import static org.sunbird.common.models.util.JsonKey.EKSTEP_BASE_URL;
 import static org.sunbird.common.models.util.JsonKey.SUNBIRD_AUTHORIZATION;
@@ -29,6 +15,21 @@ import static org.sunbird.common.models.util.ProjectUtil.getConfigValue;
 import static org.sunbird.common.responsecode.ResponseCode.SERVER_ERROR;
 import static org.sunbird.common.responsecode.ResponseCode.errorProcessingRequest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.commons.lang3.StringUtils;
+import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.response.Response;
+import org.sunbird.common.responsecode.ResponseCode;
+
 public class TextBookTocUtil {
 
   private static ObjectMapper mapper = new ObjectMapper();
@@ -36,6 +37,7 @@ public class TextBookTocUtil {
   private static Map<String, String> getHeaders() {
     Map<String, String> headers = new HashMap<>();
     headers.put(AUTHORIZATION, BEARER + getConfigValue(SUNBIRD_AUTHORIZATION));
+    headers.put("Content-Type", "application/json");
     return headers;
   }
 
@@ -68,7 +70,8 @@ public class TextBookTocUtil {
     return handleReadRequest(contentId, SUNBIRD_CONTENT_READ_API, null);
   }
 
-  private static Response handleReadRequest(String id, String urlPath, Map<String, String> requestParams) {
+  private static Response handleReadRequest(
+      String id, String urlPath, Map<String, String> requestParams) {
     Map<String, String> headers = getHeaders();
     ObjectMapper mapper = new ObjectMapper();
 
@@ -82,26 +85,49 @@ public class TextBookTocUtil {
               + id
               + requestParams(requestParams);
 
-      log("TextBookTocUtil:handleReadRequest: Sending GET Request | TextBook Id: " + id + ", Request URL: "
-              + requestUrl, INFO.name());
+      log(
+          "TextBookTocUtil:handleReadRequest: Sending GET Request | TextBook Id: "
+              + id
+              + ", Request URL: "
+              + requestUrl,
+          INFO.name());
 
-      String httpResponse = sendGetRequest(requestUrl, headers);
-      if (StringUtils.isBlank(httpResponse)) {
-        log("TextBookTocUtil:handleReadRequest: Received Empty Response | TextBook Id: " + id + ", Request URL: "
-                + requestUrl, ERROR.name());
-        throwServerErrorException(ResponseCode.SERVER_ERROR, errorProcessingRequest.getErrorMessage());
+      HttpResponse<String> httpResponse = Unirest.get(requestUrl).headers(headers).asString();
+
+      if (StringUtils.isBlank(httpResponse.getBody())) {
+        log(
+            "TextBookTocUtil:handleReadRequest: Received Empty Response | TextBook Id: "
+                + id
+                + ", Request URL: "
+                + requestUrl,
+            ERROR.name());
+        throwServerErrorException(
+            ResponseCode.SERVER_ERROR, errorProcessingRequest.getErrorMessage());
       }
-      response = mapper.readValue(httpResponse, Response.class);
+      response = mapper.readValue(httpResponse.getBody(), Response.class);
       if (!ResponseCode.OK.equals(response.getResponseCode())) {
-        log("TextBookTocUtil:handleReadRequest: Response code is not ok | TextBook Id: " + id + "| Request URL: "
-                + requestUrl, ERROR.name());
+        log(
+            "TextBookTocUtil:handleReadRequest: Response code is not ok | TextBook Id: "
+                + id
+                + "| Request URL: "
+                + requestUrl,
+            ERROR.name());
         throw new ProjectCommonException(
-                response.getParams().getErr(),
-                response.getParams().getErrmsg(),
-                response.getResponseCode().getResponseCode());
+            response.getResponseCode().name(),
+            response.getParams().getErrmsg(),
+            response.getResponseCode().getResponseCode());
       }
     } catch (IOException e) {
-      log("TextBookTocUtil:handleReadRequest: Exception occurred with error message = " + e.getMessage(), e);
+      log(
+          "TextBookTocUtil:handleReadRequest: Exception occurred with error message = "
+              + e.getMessage(),
+          e);
+      throwServerErrorException(ResponseCode.SERVER_ERROR);
+    } catch (UnirestException e) {
+      log(
+          "TextBookTocUtil:handleReadRequest: Exception occurred with error message = "
+              + e.getMessage(),
+          e);
       throwServerErrorException(ResponseCode.SERVER_ERROR);
     }
     return response;
@@ -133,9 +159,9 @@ public class TextBookTocUtil {
     } catch (JsonProcessingException e) {
       log("Error Serializing Object To String", ERROR.name());
       throw new ProjectCommonException(
-              errorProcessingRequest.getErrorCode(),
-              errorProcessingRequest.getErrorMessage(),
-              SERVER_ERROR.getResponseCode());
+          errorProcessingRequest.getErrorCode(),
+          errorProcessingRequest.getErrorMessage(),
+          SERVER_ERROR.getResponseCode());
     }
   }
 
