25496d9a6657d833fd36799b1f1bee99b1afc280
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2019-01-11 10:00:52 +0530

    Update LocationActorOperation.java

--- common-util/src/main/java/org/sunbird/common/models/util/LocationActorOperation.java
+++ common-util/src/main/java/org/sunbird/common/models/util/LocationActorOperation.java
@@ -1,32 +1,21 @@
 package org.sunbird.common.models.util;
 
-/** Created by arvind on 18/4/18. */
 public enum LocationActorOperation {
   CREATE_LOCATION("createLocation"),
   UPDATE_LOCATION("updateLocation"),
   SEARCH_LOCATION("searchLocation"),
-  GET_LOCATION_IDS("getLocationIds"),
   DELETE_LOCATION("deleteLocation"),
+  GET_RELATED_LOCATION_IDS("getRelatedLocationIds"),
   READ_LOCATION_TYPE("readLocationType"),
   UPSERT_LOCATION_TO_ES("upsertLocationDataToES"),
   DELETE_LOCATION_FROM_ES("deleteLocationDataFromES");
 
   private String value;
 
-  /**
-   * constructor
-   *
-   * @param value String
-   */
   LocationActorOperation(String value) {
     this.value = value;
   }
 
-  /**
-   * returns enum value
-   *
-   * @return
-   */
   public String getValue() {
     return this.value;
   }
