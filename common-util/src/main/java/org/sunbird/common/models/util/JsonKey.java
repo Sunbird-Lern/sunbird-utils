0ce0813ad77d235b423e8e1f38f4f6ff0b388afd
AMIT KUMAR <amit.kumar@tarento.com>
2019-02-26 12:48:51 +0530

    Issue #SB-10705 feat:validating dialcode with channel

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -840,6 +840,7 @@ public final class JsonKey {
   public static final String FRAMEWORK_READ_API_URL = "framework_read_api_url";
   public static final String DIAL_CODE_IDENTIFIER_MAP = "dialCodeIdentifierMap";
   public static final String LINK_DIAL_CODE_API = "sunbird_link_dial_code_api";
+  public static final String SUNBIRD_DIALCODE_SEARCH_API = "sunbird_dialcode_search_api";
 
   private JsonKey() {}
 }
