4c757b5121a0c111771ba364f5edf41e527605b4
rajat <rajatgupta0889@gmail.com>
2019-03-26 18:19:13 +0530

    fix divide method into smaller methods

--- common-util/src/main/java/org/sunbird/common/request/BaseRequestValidator.java
+++ common-util/src/main/java/org/sunbird/common/request/BaseRequestValidator.java
@@ -364,53 +364,11 @@ public class BaseRequestValidator {
               ResponseCode.dataTypeError.getErrorMessage(), JsonKey.FILTERS, "Map"),
           ResponseCode.CLIENT_ERROR.getResponseCode());
     }
-    if (request.getRequest().containsKey(JsonKey.FILTERS)
-        && ((request.getRequest().get(JsonKey.FILTERS) instanceof Map))) {
-      Map<String, Object> map = (Map<String, Object>) request.getRequest().get(JsonKey.FILTERS);
+    validateSearchRequestFiltersValues(request);
+    validateSearchRequestFieldsValues(request);
+  }
 
-      map.forEach(
-          (key, val) -> {
-            if (key == null) {
-              throw new ProjectCommonException(
-                  ResponseCode.invalidParameterValue.getErrorCode(),
-                  MessageFormat.format(
-                      ResponseCode.invalidParameterValue.getErrorMessage(), key, JsonKey.FILTERS),
-                  ResponseCode.CLIENT_ERROR.getResponseCode());
-            }
-            if (val instanceof List) {
-              ((List) val)
-                  .forEach(
-                      v -> {
-                        if (v == null) {
-                          throw new ProjectCommonException(
-                              ResponseCode.invalidParameterValue.getErrorCode(),
-                              MessageFormat.format(
-                                  ResponseCode.invalidParameterValue.getErrorMessage(), v, key),
-                              ResponseCode.CLIENT_ERROR.getResponseCode());
-                        }
-                      });
-            } else if (val instanceof Map) {
-              ((Map) val)
-                  .forEach(
-                      (k, v) -> {
-                        if (k == null || v == null) {
-                          throw new ProjectCommonException(
-                              ResponseCode.invalidParameterValue.getErrorCode(),
-                              MessageFormat.format(
-                                  ResponseCode.invalidParameterValue.getErrorMessage(), v, k),
-                              ResponseCode.CLIENT_ERROR.getResponseCode());
-                        }
-                      });
-            } else if (val == null)
-              if (StringUtils.isEmpty((String) val)) {
-                throw new ProjectCommonException(
-                    ResponseCode.invalidParameterValue.getErrorCode(),
-                    MessageFormat.format(
-                        ResponseCode.invalidParameterValue.getErrorMessage(), val, key),
-                    ResponseCode.CLIENT_ERROR.getResponseCode());
-              }
-          });
-    }
+  private void validateSearchRequestFieldsValues(Request request) {
     if (request.getRequest().containsKey(JsonKey.FIELDS)
         && (!(request.getRequest().get(JsonKey.FIELDS) instanceof List))) {
       throw new ProjectCommonException(
@@ -431,6 +389,60 @@ public class BaseRequestValidator {
         }
       }
     }
+  }
+
+  private void validateSearchRequestFiltersValues(Request request) {
+    if (request.getRequest().containsKey(JsonKey.FILTERS)
+        && ((request.getRequest().get(JsonKey.FILTERS) instanceof Map))) {
+      Map<String, Object> map = (Map<String, Object>) request.getRequest().get(JsonKey.FILTERS);
+
+      map.forEach(
+          (key, val) -> {
+            if (key == null) {
+              throw new ProjectCommonException(
+                  ResponseCode.invalidParameterValue.getErrorCode(),
+                  MessageFormat.format(
+                      ResponseCode.invalidParameterValue.getErrorMessage(), key, JsonKey.FILTERS),
+                  ResponseCode.CLIENT_ERROR.getResponseCode());
+            }
+            if (val instanceof List) {
+              validateListValues((List) val, key);
+            } else if (val instanceof Map) {
+              validateMapValues((Map) val);
+            } else if (val == null)
+              if (StringUtils.isEmpty((String) val)) {
+                throw new ProjectCommonException(
+                    ResponseCode.invalidParameterValue.getErrorCode(),
+                    MessageFormat.format(
+                        ResponseCode.invalidParameterValue.getErrorMessage(), val, key),
+                    ResponseCode.CLIENT_ERROR.getResponseCode());
+              }
+          });
+    }
+  }
+
+  private void validateMapValues(Map val) {
+    val.forEach(
+        (k, v) -> {
+          if (k == null || v == null) {
+            throw new ProjectCommonException(
+                ResponseCode.invalidParameterValue.getErrorCode(),
+                MessageFormat.format(ResponseCode.invalidParameterValue.getErrorMessage(), v, k),
+                ResponseCode.CLIENT_ERROR.getResponseCode());
+          }
+        });
+  }
+
+  private void validateListValues(List val, String key) {
+    val.forEach(
+        v -> {
+          if (v == null) {
+            throw new ProjectCommonException(
+                ResponseCode.invalidParameterValue.getErrorCode(),
+                MessageFormat.format(ResponseCode.invalidParameterValue.getErrorMessage(), v, key),
+                ResponseCode.CLIENT_ERROR.getResponseCode());
+          }
+        });
   }
 
   public void validateEmail(String email) {
