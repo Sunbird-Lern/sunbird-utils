6ec2145cec10fe89f789dbba6fe806b98e7b66a1
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2019-04-09 17:08:15 +0530

    Update Trigger.java

--- cassandra-trigger/src/main/java/org/sunbird/cassandra/Trigger.java
+++ cassandra-trigger/src/main/java/org/sunbird/cassandra/Trigger.java
@@ -31,7 +31,6 @@ import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 import org.apache.cassandra.triggers.ITrigger;
 import org.sunbird.common.audit.AuditUtil;
 
-/** @author iostream04 */
 public class Trigger implements ITrigger {
 
   private static final String KEYSPACE = "keyspace";
@@ -40,122 +39,100 @@ public class Trigger implements ITrigger {
   private static final String UPDATE_ROW = "UPDATE_ROW";
   private static final String DELETE_ROW = "DELETE_ROW";
   private static final String UPDATE_COUNTER = "UPDATE_COUNTER";
-  private static final String FILE_TO_WRITE = "/home/sudhirgiri/triggerLogs.txt";
+  private static final String FILE_TO_WRITE = "/var/log/cassandra/triggerAuditLog.log";
 
   @Override
   public Collection<Mutation> augment(Partition update) {
-
     Map<String, Object> resultMap = processData(update);
-    Map<String, Object> auditMap = AuditUtil.getAuditForTransanctional(resultMap);
+    Map<String, Object> auditEventMap = AuditUtil.getAuditEvent(resultMap);
     try {
-      // Map<String, Object> result  = new ObjectMapper().convertValue(audit, Map.class);
       BufferedWriter out = new BufferedWriter(new FileWriter(FILE_TO_WRITE, true));
-      out.write(auditMap + "\n");
-      out.write("-------" + "\n");
+      out.write(auditEventMap + "\n");
       out.close();
     } catch (IOException e) {
-      System.out.println("ExceptionOccured");
+      System.out.println("Trigger:augment: Exception occurred with error message = " + e.getMessage());
     }
     return null;
   }
 
-  private HashMap<String, Object> getPartitionKeyData(ByteBuffer key, CFMetaData update) {
+  private HashMap<String, Object> getPartitionKeyData(ByteBuffer keyValueBuffer, CFMetaData metadata) {
+    List<ColumnDefinition> partitionKeyColumns = metadata.partitionKeyColumns();
+    Map<String, Object> partitionKeyValueMap = new HashMap<>();
 
-    List<ColumnDefinition> partitionKeyList = new ArrayList<ColumnDefinition>();
-    HashMap<String, Object> partitionKeyValueList = new HashMap<String, Object>();
-    Object dataObject = null;
-    partitionKeyList = update.partitionKeyColumns();
-    if (partitionKeyList.size() == 1) {
+    partitionKeyValueMap.put(TABLE, metadata.cfName);
+    partitionKeyValueMap.put(KEYSPACE, metadata.ksName);
 
-      AbstractType<?> ptype = partitionKeyList.get(0).type;
-      dataObject = ptype.compose(key);
+    Object pkValue = null;
+    
+    for (int index = 0; index < partitionKeyColumns.size(); index++) {
+        String pkColumnName = partitionKeyColumns.get(index).name.toString();
+        AbstractType<?> pkColumnType = partitionKeyColumns.get(index).type;
 
-      if (dataObject != null) {
-        partitionKeyValueList.put(partitionKeyList.get(0).name.toString(), dataObject);
-      }
-    } else {
-      for (int pkIndex = 0; pkIndex < partitionKeyList.size(); pkIndex++) {
-        String pname = partitionKeyList.get(pkIndex).name.toString();
-        AbstractType<?> ptype = partitionKeyList.get(pkIndex).type;
-
-        ByteBuffer tempKey = CompositeType.extractComponent(key, pkIndex);
-        dataObject = ptype.compose(tempKey);
+        ByteBuffer valueBuffer = CompositeType.extractComponent(keyValueBuffer, index);
+        pkValue = ptype.compose(valueBuffer);
 
         if (dataObject != null) {
-          partitionKeyValueList.put(pname, dataObject);
+          partitionKeyValueMap.put(pkColumnName, pkValue);
         }
       }
-    }
 
-    return partitionKeyValueList;
+    return partitionKeyValueMap;
   }
 
   private Map<String, Object> getClusterKeyData(Partition update, Unfiltered next) {
-    List<ColumnDefinition> clusterKeyList = new ArrayList<ColumnDefinition>();
-    Map<String, Object> clusterKeyValueList = new HashMap<String, Object>();
+    Map<String, Object> clusterKeyValueMap = new HashMap<>();
 
-    clusterKeyList = update.metadata().clusteringColumns();
+    List<ColumnDefinition> clusteringColumns = update.metadata().clusteringColumns();
     ClusteringPrefix clustering = next.clustering();
-    int clSize = clustering.size();
-    for (int i = 0; i < clSize; i++) {
-      ColumnDefinition columnDefinition = clusterKeyList.get(i);
+
+    for (int index = 0; index < clustering.size(); index++) {
+      ColumnDefinition columnDefinition = clusterKeyList.get(index);
+
       String columnName = columnDefinition.name.toString();
       AbstractType<?> columnType = columnDefinition.type;
-      Object compose = columnType.compose(clustering.get(i));
-      clusterKeyValueList.put(columnName, compose);
+
+      Object ckValue = columnType.compose(clustering.get(index));
+      clusterKeyValueMap.put(columnName, ckValue);
     }
-    return clusterKeyValueList;
+
+    return clusterKeyValueMap;
   }
 
   public HashMap<String, Object> processData(Partition partition) {
     String updateType = null;
 
     try {
-      final String table = partition.metadata().cfName;
-      final String keyspace = partition.metadata().ksName;
-      HashMap<String, Object> partitionKeyData = new HashMap<String, Object>();
-      Map<String, Object> clusterKeyData = new HashMap<String, Object>();
       DecoratedKey partitionKey = partition.partitionKey();
-      partitionKeyData = getPartitionKeyData(partitionKey.getKey(), partition.metadata());
 
+      HashMap<String, Object> partitionKeyData = getPartitionKeyData(partitionKey.getKey(), partition.metadata());
+
+      UnfilteredRowIterator unfilteredIterator = partition.unfilteredIterator();
+      Unfiltered next = unfilteredIterator.next();
+      Map<String, Object> clusterKeyData = getClusterKeyData(partition, next);
+
+      // Is delete operation?
       DeletionTime levelDeletion = partition.partitionLevelDeletion();
-
       if (!levelDeletion.isLive()) {
-
-        updateType = DELETE_ROW;
-        partitionKeyData.put(OPERATION_TYPE, updateType);
-        partitionKeyData.put(TABLE, table);
-        partitionKeyData.put(KEYSPACE, keyspace);
+        partitionKeyData.put(OPERATION_TYPE, DELETE_ROW);
         return partitionKeyData;
       }
 
-      UnfilteredRowIterator unfilteredIterator = partition.unfilteredIterator();
+      ClusteringPrefix clustering = next.clustering();
+      updateType = UPDATE_ROW;
+
       while (unfilteredIterator.hasNext()) {
-        updateType = UPDATE_ROW;
-
-        Unfiltered next = unfilteredIterator.next();
-
-        ClusteringPrefix clustering = next.clustering();
         Row row = partition.getRow((Clustering) clustering);
-        clusterKeyData = getClusterKeyData(partition, next);
 
         Iterable<Cell> cells = row.cells();
 
-        final Map<Object, String> updateColumnCollectionInfo = new HashMap<Object, String>();
-        HashMap<String, Object> dataMap = new HashMap<String, Object>();
-        HashMap<Object, Object> deletedDataMap = new HashMap<Object, Object>();
-
-        Deletion deletion = row.deletion();
-
-        if (deletion != null && !deletion.isLive()) {
-          updateType = DELETE_ROW;
-        } else {
+        final Map<Object, String> updateColumnCollectionInfo = new HashMap<>();
+        HashMap<String, Object> dataMap = new HashMap<>();
+        HashMap<Object, Object> deletedDataMap = new HashMap<>();
 
           for (Cell cell : cells) {
 
             ColumnDefinition column = cell.column();
             String columnName = column.name + "";
-            // AbstractType<?> type = column.type;
             AbstractType<Object> cellValueType = (AbstractType<Object>) column.cellValueType();
             Object cellValue = cellValueType.compose(cell.value());
             AbstractType<?> columnType = column.type;
@@ -251,7 +228,6 @@ public class Trigger implements ITrigger {
                 dataMap.put(columnName, null);
               }
             }
-          }
         }
         dataMap.put(OPERATION_TYPE, updateType);
         dataMap.put(TABLE, table);
