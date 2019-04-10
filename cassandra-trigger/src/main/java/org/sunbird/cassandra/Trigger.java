5e0f19f7bc15dbc35addcaac465246e980e0a32b
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2019-04-10 09:21:02 +0530

    Issue #SB-11593 feat:Added trigger

--- cassandra-trigger/src/main/java/org/sunbird/cassandra/Trigger.java
+++ cassandra-trigger/src/main/java/org/sunbird/cassandra/Trigger.java
@@ -25,7 +25,6 @@ import org.apache.cassandra.db.partitions.Partition;
 import org.apache.cassandra.db.rows.Cell;
 import org.apache.cassandra.db.rows.CellPath;
 import org.apache.cassandra.db.rows.Row;
-import org.apache.cassandra.db.rows.Row.Deletion;
 import org.apache.cassandra.db.rows.Unfiltered;
 import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 import org.apache.cassandra.triggers.ITrigger;
@@ -38,24 +37,33 @@ public class Trigger implements ITrigger {
   private static final String OPERATION_TYPE = "operationType";
   private static final String UPDATE_ROW = "UPDATE_ROW";
   private static final String DELETE_ROW = "DELETE_ROW";
-  private static final String UPDATE_COUNTER = "UPDATE_COUNTER";
   private static final String FILE_TO_WRITE = "/var/log/cassandra/triggerAuditLog.log";
 
   @Override
   public Collection<Mutation> augment(Partition update) {
     Map<String, Object> resultMap = processData(update);
     Map<String, Object> auditEventMap = AuditUtil.getAuditEvent(resultMap);
+    BufferedWriter out = null;
     try {
-      BufferedWriter out = new BufferedWriter(new FileWriter(FILE_TO_WRITE, true));
+      out = new BufferedWriter(new FileWriter(FILE_TO_WRITE, true));
       out.write(auditEventMap + "\n");
-      out.close();
     } catch (IOException e) {
-      System.out.println("Trigger:augment: Exception occurred with error message = " + e.getMessage());
+      System.out.println(
+          "Trigger:augment: Exception occurred with error message = " + e.getMessage());
+    } finally {
+      if (out != null) {
+        try {
+          out.close();
+        } catch (IOException e) {
+          System.out.println(
+              "Trigger:augment: Exception occured while closing the audit file " + e.getMessage());
+        }
+      }
     }
     return null;
   }
 
-  private HashMap<String, Object> getPartitionKeyData(ByteBuffer keyValueBuffer, CFMetaData metadata) {
+  private Map<String, Object> getPartitionKeyData(ByteBuffer keyValueBuffer, CFMetaData metadata) {
     List<ColumnDefinition> partitionKeyColumns = metadata.partitionKeyColumns();
     Map<String, Object> partitionKeyValueMap = new HashMap<>();
 
@@ -63,18 +71,18 @@ public class Trigger implements ITrigger {
     partitionKeyValueMap.put(KEYSPACE, metadata.ksName);
 
     Object pkValue = null;
-    
+
     for (int index = 0; index < partitionKeyColumns.size(); index++) {
-        String pkColumnName = partitionKeyColumns.get(index).name.toString();
-        AbstractType<?> pkColumnType = partitionKeyColumns.get(index).type;
+      String pkColumnName = partitionKeyColumns.get(index).name.toString();
+      AbstractType<?> pkColumnType = partitionKeyColumns.get(index).type;
 
-        ByteBuffer valueBuffer = CompositeType.extractComponent(keyValueBuffer, index);
-        pkValue = ptype.compose(valueBuffer);
+      ByteBuffer valueBuffer = CompositeType.extractComponent(keyValueBuffer, index);
+      pkValue = pkColumnType.compose(valueBuffer);
 
-        if (dataObject != null) {
-          partitionKeyValueMap.put(pkColumnName, pkValue);
-        }
+      if (pkValue != null) {
+        partitionKeyValueMap.put(pkColumnName, pkValue);
       }
+    }
 
     return partitionKeyValueMap;
   }
@@ -86,7 +94,7 @@ public class Trigger implements ITrigger {
     ClusteringPrefix clustering = next.clustering();
 
     for (int index = 0; index < clustering.size(); index++) {
-      ColumnDefinition columnDefinition = clusterKeyList.get(index);
+      ColumnDefinition columnDefinition = clusteringColumns.get(index);
 
       String columnName = columnDefinition.name.toString();
       AbstractType<?> columnType = columnDefinition.type;
@@ -98,13 +106,14 @@ public class Trigger implements ITrigger {
     return clusterKeyValueMap;
   }
 
-  public HashMap<String, Object> processData(Partition partition) {
+  public Map<String, Object> processData(Partition partition) {
     String updateType = null;
 
     try {
       DecoratedKey partitionKey = partition.partitionKey();
 
-      HashMap<String, Object> partitionKeyData = getPartitionKeyData(partitionKey.getKey(), partition.metadata());
+      Map<String, Object> partitionKeyData =
+          getPartitionKeyData(partitionKey.getKey(), partition.metadata());
 
       UnfilteredRowIterator unfilteredIterator = partition.unfilteredIterator();
       Unfiltered next = unfilteredIterator.next();
@@ -114,6 +123,7 @@ public class Trigger implements ITrigger {
       DeletionTime levelDeletion = partition.partitionLevelDeletion();
       if (!levelDeletion.isLive()) {
         partitionKeyData.put(OPERATION_TYPE, DELETE_ROW);
+        partitionKeyData.putAll(clusterKeyData);
         return partitionKeyData;
       }
 
@@ -125,113 +135,27 @@ public class Trigger implements ITrigger {
 
         Iterable<Cell> cells = row.cells();
 
-        final Map<Object, String> updateColumnCollectionInfo = new HashMap<>();
-        HashMap<String, Object> dataMap = new HashMap<>();
-        HashMap<Object, Object> deletedDataMap = new HashMap<>();
-
-          for (Cell cell : cells) {
-
-            ColumnDefinition column = cell.column();
-            String columnName = column.name + "";
-            AbstractType<Object> cellValueType = (AbstractType<Object>) column.cellValueType();
-            Object cellValue = cellValueType.compose(cell.value());
-            AbstractType<?> columnType = column.type;
-            if (cell.isCounterCell()) {
-              updateType = UPDATE_COUNTER;
-              dataMap.put(columnName, cellValue);
-            } else if (columnType instanceof MapType<?, ?>) {
-              MapType<Object, Object> mapType = (MapType<Object, Object>) columnType;
-
-              AbstractType<Object> keysType = mapType.getKeysType();
-              CellPath path = cell.path();
-              int size = path.size();
-              for (int i = 0; i < size; i++) {
-                ByteBuffer byteBuffer = path.get(i);
-                Object cellKey = keysType.compose(byteBuffer);
-
-                if (!dataMap.containsKey(columnName)) {
-                  Map<Object, Object> map = new HashMap<Object, Object>();
-                  dataMap.put(columnName, map);
-
-                  if (cell.isLive(0)) {
-                    map.put(cellKey.toString(), cellValue);
-                  } else {
-                    if (!updateColumnCollectionInfo.containsKey(columnName)) {
-                      updateColumnCollectionInfo.put(columnName, columnType.getClass().getName());
-                    }
-                    map.put(cellKey.toString(), null);
-                  }
-                } else {
-                  Map<Object, Object> map = (Map<Object, Object>) dataMap.get(columnName);
-
-                  if (cell.isLive(0)) {
-                    map.put(cellKey.toString(), cellValue);
-                  } else {
-                    if (!updateColumnCollectionInfo.containsKey(columnName)) {
-                      updateColumnCollectionInfo.put(columnName, columnType.getClass().getName());
-                    }
-                    map.put(cellKey.toString(), null);
-                  }
-                }
-              }
-            } else if (columnType instanceof SetType<?>) {
-              CellPath path = cell.path();
-              int size = path.size();
-              for (int i = 0; i < size; i++) {
-                ByteBuffer byteBuffer = path.get(i);
-                AbstractType<Object> keysType = ((SetType) columnType).getElementsType();
-                cellValue = keysType.compose(byteBuffer);
-              }
-              updateColumnCollectionInfo.put(columnName, columnType.getClass().getName());
-              if (cell.isLive(0)) {
-                if (!dataMap.containsKey(columnName)) {
-                  ArrayList<Object> arrayList = new ArrayList<Object>();
-                  arrayList.add(cellValue);
-                  dataMap.put(columnName, arrayList);
-                } else {
-                  ArrayList<Object> arrayList = (ArrayList<Object>) dataMap.get(columnName);
-                  if (!arrayList.contains(cellValue)) {
-                    arrayList.add(cellValue);
-                  }
-                }
-              } else {
-
-                if (!deletedDataMap.containsKey(columnName)) {
-                  ArrayList<Object> arrayList = new ArrayList<Object>();
-                  arrayList.add(cellValue);
-                  deletedDataMap.put(columnName, arrayList);
-                } else {
-                  ArrayList<Object> arrayList = (ArrayList<Object>) deletedDataMap.get(columnName);
-                  if (!arrayList.contains(cellValue)) {
-                    arrayList.add(cellValue);
-                  }
-                }
-              }
-            } else if (columnType instanceof ListType<?>) {
-              updateColumnCollectionInfo.put(columnName, columnType.getClass().getName());
-              if (cell.isLive(0)) {
-                if (!dataMap.containsKey(columnName)) {
-                  ArrayList<Object> arrayList = new ArrayList<Object>();
-                  arrayList.add(cellValue);
-                  dataMap.put(columnName, arrayList);
-                } else {
-                  ArrayList<Object> arrayList = (ArrayList<Object>) dataMap.get(columnName);
-                  if (!arrayList.contains(cellValue)) {
-                    arrayList.add(cellValue);
-                  }
-                }
-              }
+        Map<String, Object> dataMap = new HashMap<>();
+        Map<Object, String> updateColumnCollectionInfo = new HashMap<Object, String>();
+        Map<Object, Object> deletedDataMap = new HashMap<>();
+        for (Cell cell : cells) {
+          AbstractType<?> columnType = getColumnType(cell);
+          if (columnType instanceof MapType<?, ?>) {
+            processMapDataType(dataMap, updateColumnCollectionInfo, cell);
+          } else if (columnType instanceof SetType<?>) {
+            processSetDataType(dataMap, deletedDataMap, cell);
+          } else if (columnType instanceof ListType<?>) {
+            processListDataType(dataMap, updateColumnCollectionInfo, cell);
+          } else {
+            String columnName = getColumnName(cell);
+            if (cell.isLive(0)) {
+              dataMap.put(columnName, getCellValue(cell));
             } else {
-              if (cell.isLive(0)) {
-                dataMap.put(columnName, cellValue);
-              } else {
-                dataMap.put(columnName, null);
-              }
+              dataMap.put(columnName, null);
             }
+          }
         }
         dataMap.put(OPERATION_TYPE, updateType);
-        dataMap.put(TABLE, table);
-        dataMap.put(KEYSPACE, keyspace);
         dataMap.putAll(partitionKeyData);
         dataMap.putAll(clusterKeyData);
         return dataMap;
@@ -241,5 +165,105 @@ public class Trigger implements ITrigger {
     }
 
     return null;
+  }
+
+  private void processMapDataType(
+      Map<String, Object> dataMap, Map<Object, String> updateColumnCollectionInfo, Cell cell) {
+    String columnName = getColumnName(cell);
+    AbstractType<?> columnType = getColumnType(cell);
+    Object cellValue = getCellValue(cell);
+    MapType<Object, Object> mapType = (MapType<Object, Object>) columnType;
+
+    AbstractType<Object> keysType = mapType.getKeysType();
+    CellPath path = cell.path();
+    int size = path.size();
+    for (int i = 0; i < size; i++) {
+      ByteBuffer byteBuffer = path.get(i);
+      Object cellKey = keysType.compose(byteBuffer);
+      Map<Object, Object> map = new HashMap<Object, Object>();
+      if (!dataMap.containsKey(columnName)) {
+        dataMap.put(columnName, map);
+      } else {
+        map = (Map<Object, Object>) dataMap.get(columnName);
+      }
+      if (cell.isLive(0)) {
+        map.put(cellKey.toString(), cellValue);
+      } else {
+        if (!updateColumnCollectionInfo.containsKey(columnName)) {
+          updateColumnCollectionInfo.put(columnName, columnType.getClass().getName());
+        }
+        map.put(cellKey.toString(), null);
+      }
+    }
+  }
+
+  private void processSetDataType(
+      Map<String, Object> dataMap, Map<Object, Object> deletedDataMap, Cell cell) {
+    String columnName = getColumnName(cell);
+    Object cellValue = getCellValue(cell);
+    AbstractType<?> columnType = getColumnType(cell);
+    MapType<Object, Object> mapType = (MapType<Object, Object>) columnType;
+    CellPath path = cell.path();
+    int size = path.size();
+    for (int i = 0; i < size; i++) {
+      ByteBuffer byteBuffer = path.get(i);
+      AbstractType<Object> keysType = ((SetType) columnType).getElementsType();
+      cellValue = keysType.compose(byteBuffer);
+    }
+    if (cell.isLive(0)) {
+      if (!dataMap.containsKey(columnName)) {
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add(cellValue);
+        dataMap.put(columnName, arrayList);
+      } else {
+        ArrayList<Object> arrayList = (ArrayList<Object>) dataMap.get(columnName);
+        if (!arrayList.contains(cellValue)) {
+          arrayList.add(cellValue);
+        }
+      }
+    } else {
+      if (!deletedDataMap.containsKey(columnName)) {
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add(cellValue);
+        deletedDataMap.put(columnName, arrayList);
+      } else {
+        ArrayList<Object> arrayList = (ArrayList<Object>) deletedDataMap.get(columnName);
+        if (!arrayList.contains(cellValue)) {
+          arrayList.add(cellValue);
+        }
+      }
+    }
+  }
+
+  private void processListDataType(
+      Map<String, Object> dataMap, Map<Object, String> updateColumnCollectionInfo, Cell cell) {
+    String columnName = getColumnName(cell);
+    Object cellValue = getCellValue(cell);
+    AbstractType<?> columnType = getColumnType(cell);
+    updateColumnCollectionInfo.put(columnName, columnType.getClass().getName());
+    if (cell.isLive(0)) {
+      if (!dataMap.containsKey(columnName)) {
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add(cellValue);
+        dataMap.put(columnName, arrayList);
+      } else {
+        ArrayList<Object> arrayList = (ArrayList<Object>) dataMap.get(columnName);
+        if (!arrayList.contains(cellValue)) {
+          arrayList.add(cellValue);
+        }
+      }
+    }
+  }
+
+  private String getColumnName(Cell cell) {
+    return cell.column().name + "";
+  }
+
+  private Object getCellValue(Cell cell) {
+    return ((AbstractType<Object>) cell.column().cellValueType()).compose(cell.value());
+  }
+
+  private AbstractType<?> getColumnType(Cell cell) {
+    return cell.column().type;
   }
 }
