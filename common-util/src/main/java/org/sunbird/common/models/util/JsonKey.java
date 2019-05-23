9ea3a9e91c4be60c14f3441ea5b80e07e8b2156e
anmol2302 <anmolgupta23021997@gmail.com>
2019-05-23 16:37:39 +0530

    Jsonkey Telemetrysource added

--- common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -905,7 +905,8 @@ public final class JsonKey {
   public static final String SUNBIRD_CACHE_ENABLE = "sunbird_cache_enable";
   public static final String MAP_NAME = "mapName";
   public static final String PAGE_ASSEMBLE = "pageAssemble";
-  public static final String signupType = "signupType";
+  public static final String SIGNUP_TYPE = "signupType";
+  public static final String TELEMETRY_SOURCE = "source";
 
   private JsonKey() {}
 }
