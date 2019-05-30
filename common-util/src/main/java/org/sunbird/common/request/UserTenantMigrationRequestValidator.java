efe3841afedd7df70409a3452be08f2f4c86ceaa
AMIT KUMAR <amit.kumar@tarento.com>
2019-05-30 19:57:12 +0530

    Issue #SB-12693 feat: user tenant migration

---
+++ common-util/src/main/java/org/sunbird/common/request/UserTenantMigrationRequestValidator.java
@@ -0,0 +1,16 @@
+package org.sunbird.common.request;
+
+import java.util.Map;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.responsecode.ResponseCode;
+
+public class UserTenantMigrationRequestValidator extends BaseRequestValidator {
+
+  public void validateUserTenantMigrateRequest(Request request) {
+    Map<String, Object> req = request.getRequest();
+    validateParam(
+        (String) req.get(JsonKey.CHANNEL), ResponseCode.mandatoryParamsMissing, JsonKey.CHANNEL);
+    validateParam(
+        (String) req.get(JsonKey.USER_ID), ResponseCode.mandatoryParamsMissing, JsonKey.USER_ID);
+  }
+}
