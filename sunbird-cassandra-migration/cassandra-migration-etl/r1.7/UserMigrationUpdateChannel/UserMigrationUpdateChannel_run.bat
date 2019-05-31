fb2c6a1cc40b84e69190b2998bff48e3c72669b9
Rahul K <rahul.kumar@tarento.com>
2019-05-30 23:05:15 -0700

    util refactoring and renaming

--- sunbird-cassandra-migration/cassandra-migration-etl/r1.7/UserMigrationUpdateChannel/UserMigrationUpdateChannel_run.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/cassandra-driver-core-3.0.0-shaded.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.15.jar;../lib/log4j-1.2.16.jar;../lib/metrics-core-3.1.2.jar;../lib/slf4j-api-1.7.7.jar;../lib/talend-cassandra-1.2.jar;../lib/talendcsv.jar;../lib/trove.jar;usermigrationupdatechannel_0_1.jar; userexternalidmigration.usermigrationupdatechannel_0_1.UserMigrationUpdateChannel  --context=Default %* 
--- sunbird-cassandra-migration/cassandra-migration-etl/r1.7/UserMigrationUpdateChannel/UserMigrationUpdateChannel_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/advancedPersistentLookupLib-1.2.jar:$ROOT_PATH/../lib/cassandra-driver-core-3.0.0-shaded.jar:$ROOT_PATH/../lib/commons-collections-3.2.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/jboss-serialization.jar:$ROOT_PATH/../lib/log4j-1.2.15.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/metrics-core-3.1.2.jar:$ROOT_PATH/../lib/slf4j-api-1.7.7.jar:$ROOT_PATH/../lib/talend-cassandra-1.2.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/../lib/trove.jar:$ROOT_PATH/usermigrationupdatechannel_0_1.jar: userexternalidmigration.usermigrationupdatechannel_0_1.UserMigrationUpdateChannel  --context=Default "$@" 
