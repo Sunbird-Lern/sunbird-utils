36c8ce5de6c7ebf026dc0c8d38bcf25510ef77a6
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-10 11:57:28 +0530

    Issue#SC-1132 externalId-decryption changed to decryptionUtils

--- externalid-decryption-script/src/main/java/org/sunbird/externaliddecryption/connection/factory/ConnectionFactory.java
+++
@@ -1,9 +0,0 @@
-package org.sunbird.externaliddecryption.connection.factory;
-
-import org.sunbird.externaliddecryption.connection.Connection;
-
-/** @author anmolgupta */
-public interface ConnectionFactory {
-
-  Connection getConnection(String hostName, String keyspaceName, String port);
-}
