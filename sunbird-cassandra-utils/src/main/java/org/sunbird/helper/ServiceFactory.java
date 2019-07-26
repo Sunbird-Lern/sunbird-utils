6fb5e563182094412fa06ee86d5aa3bc09998f2e
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-26 12:25:33 +0530

    Issue#SB-13808 made service factory class sengleton

--- sunbird-cassandra-utils/src/main/java/org/sunbird/helper/ServiceFactory.java
+++ sunbird-cassandra-utils/src/main/java/org/sunbird/helper/ServiceFactory.java
@@ -1,31 +1,41 @@
-/** */
+/**
+ *
+ */
 package org.sunbird.helper;
 
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.cassandraimpl.CassandraOperationImpl;
 
-import javax.inject.Singleton;
 
 /**
  * This class will provide cassandraOperationImpl instance.
  *
  * @author Manzarul
  */
-@Singleton
 public class ServiceFactory {
-  private static CassandraOperation operation = null;
+    private static volatile CassandraOperation operation = null;
 
-  private ServiceFactory() {}
-
-  /**
-   * On call of this method , it will provide a new CassandraOperationImpl instance on each call.
-   *
-   * @return
-   */
-  public static CassandraOperation getInstance() {
-    if (null == operation) {
-      operation = new CassandraOperationImpl();
+    private ServiceFactory() {
     }
-    return operation;
-  }
+
+    /**
+     * On call of this method , it will provide a new CassandraOperationImpl instance on each call.
+     *
+     * @return
+     */
+    public static CassandraOperation getInstance() {
+        if (null == operation) {
+            synchronized (ServiceFactory.class) {
+                if (null == operation) {
+                    operation = new CassandraOperationImpl();
+                }
+            }
+        }
+        return operation;
+    }
+
+    public CassandraOperation readResolve() {
+        return getInstance();
+    }
+
 }
