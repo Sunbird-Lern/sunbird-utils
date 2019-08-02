6262d5ec25c67467c617ea98b2ebb30acbddc370
anmol2302 <anmolgupta23021997@gmail.com>
2019-08-02 16:06:20 +0530

    Issue#SC-1257 added prevUsedPhone and prevUsedEmail in encryption and decryption properties

--- sunbird-platform-core/common-util/src/main/java/org/sunbird/common/responsecode/ResponseCode.java
+++ sunbird-platform-core/common-util/src/main/java/org/sunbird/common/responsecode/ResponseCode.java
@@ -808,7 +808,10 @@ public enum ResponseCode {
   REDIRECTION_REQUIRED(302),
   TOO_MANY_REQUESTS(429),
   SERVICE_UNAVAILABLE(503),
-  PARAM_NOT_MATCH(ResponseMessage.Key.PARAM_NOT_MATCH,ResponseMessage.Message.PARAM_NOT_MATCH);
+  PARAM_NOT_MATCH(ResponseMessage.Key.PARAM_NOT_MATCH,ResponseMessage.Message.PARAM_NOT_MATCH),
+  PARTIAL_SUCCESS_RESPONSE(206);
+
+
 
   private int responseCode;
   /** error code contains String value */
