da1660e39335de8fbad72db30ed8317250c61021
manzarul Haque <manzarul07@gmail.com>
2019-08-30 11:26:11 +0530

    Update Matcher.java

--- sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/Matcher.java
+++ sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/Matcher.java
@@ -13,10 +13,10 @@ public class Matcher {
      * this method will match the two arguments , equal or not
      * if two string is null or empty this method will return true
      * @param firstVal
-     * @param secondeVal
+     * @param secondVal
      * @return boolean
      */
-    public static boolean matchIdentifiers(String firstVal,String secondeVal){
-        return StringUtils.equalsIgnoreCase(firstVal,secondeVal);
+    public static boolean matchIdentifiers(String firstVal,String secondVal){
+        return StringUtils.equalsIgnoreCase(firstVal,secondVal);
     }
 }
