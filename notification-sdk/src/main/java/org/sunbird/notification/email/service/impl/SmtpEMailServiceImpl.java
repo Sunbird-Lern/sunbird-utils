be1f63bc0c005289639676637b6cb7c4b3b7b8c6
manzarul Haque <manzarul07@gmail.com>
2019-10-14 15:54:54 +0530

    Sb 14975 (#684)
    * Issue #SB-14975 feat: adding functionality for sending OTP, resend otp and verify otp
    
    * Isssue #SB-14975 feat:adding unit test for otp
    
    * Isssue #SB-14975 feat:changes for method doc
    
    * Issue #SB-14975 fix: adding log4j for logging
    
    * Issue #SB-14975 fix: adding log4j for logging
    
    * issue #SB-15032 fix: adding for sending sms , otp and verify otp
    
    * issue #SB-14975 fix: email code refactor
    
    * issue #SB-14975 fix: changes for email service
    
    * issue #Sb-14975 fix: changes for bcc email

--- notification-sdk/src/main/java/org/sunbird/notification/email/service/impl/SmtpEMailServiceImpl.java
+++ notification-sdk/src/main/java/org/sunbird/notification/email/service/impl/SmtpEMailServiceImpl.java
@@ -22,14 +22,18 @@ public class SmtpEMailServiceImpl implements IEmailService {
     email = new Email(config);
   }
 
-  @Override
+   @Override
   public boolean sendEmail(EmailRequest emailReq) {
     if (emailReq == null) {
       logger.info("Email request is null or empty:");
       return false;
-    } else if (CollectionUtils.isNotEmpty(emailReq.getBcc())) {
+      // either email object has bcc or to list size more than 1 then pass it as bcc.
+    } else if (CollectionUtils.isNotEmpty(emailReq.getBcc()) || emailReq.getTo().size() > 1) {
       return email.sendEmail(
-          email.getFromEmail(), emailReq.getSubject(), emailReq.getBody(), emailReq.getBcc());
+          email.getFromEmail(),
+          emailReq.getSubject(),
+          emailReq.getBody(),
+          CollectionUtils.isEmpty(emailReq.getBcc()) ? emailReq.getTo() : emailReq.getBcc());
     } else if (CollectionUtils.isNotEmpty(emailReq.getCc())) {
       return email.sendMail(
           emailReq.getTo(), emailReq.getSubject(), emailReq.getBody(), emailReq.getCc());
