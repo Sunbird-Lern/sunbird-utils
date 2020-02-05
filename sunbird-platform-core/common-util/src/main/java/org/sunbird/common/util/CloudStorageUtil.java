16fad27e4295f996a29a14df80781023d3714a99
anmol2302 <anmolgupta23021997@gmail.com>
2020-02-05 16:15:44 +0530

    Issue#SC-1699 feat: updating cloud lib version and removing cassandra slow query log

--- sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/CloudStorageUtil.java
+++ sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/CloudStorageUtil.java
@@ -49,14 +49,13 @@ public class CloudStorageUtil {
     IStorageService storageService = getStorageService(storageType);
 
     return storageService.upload(
-        container,
-        filePath,
-        objectKey,
-        Option.apply(false),
-        Option.apply(false),
-        Option.empty(),
-        Option.apply(STORAGE_SERVICE_API_RETRY_COUNT),
-        1);
+            container,
+            filePath,
+            objectKey,
+            Option.apply(false),
+            Option.apply(1),
+            Option.apply(STORAGE_SERVICE_API_RETRY_COUNT),
+            Option.apply(1));
   }
 
   public static String getSignedUrl(
