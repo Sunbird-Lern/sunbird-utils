f19bc6d3404fed3ccea70d866d0917a2bd38a798
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-05-14 22:45:45 +0530

    Issue #000 fix: disable total telemetry.

--- common-util/src/main/java/org/sunbird/telemetry/util/TelemetryLmaxWriter.java
+++ common-util/src/main/java/org/sunbird/telemetry/util/TelemetryLmaxWriter.java
@@ -1,9 +1,5 @@
 package org.sunbird.telemetry.util;
 
-import com.lmax.disruptor.dsl.Disruptor;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.request.Request;
 
 /**
@@ -13,14 +9,10 @@ import org.sunbird.common.request.Request;
  */
 public class TelemetryLmaxWriter {
 
-  private static Disruptor<Request> disruptor;
-  private WriteEventProducer writeEventProducer;
-  private int ringBufferSize;
   private static TelemetryLmaxWriter lmaxWriter;
 
   private TelemetryLmaxWriter() {
-    init();
-    registerShutDownHook();
+
   }
 
   /**
@@ -42,43 +34,7 @@ public class TelemetryLmaxWriter {
   }
 
   public void setRingBufferSize(int ringBufferSize) {
-    this.ringBufferSize = ringBufferSize;
-  }
 
-  /** Initialize the disruptor engine. */
-  @SuppressWarnings("unchecked")
-  private void init() {
-    // create a thread pool executor to be used by disruptor
-    Executor executor = Executors.newCachedThreadPool();
-
-    // initialize our event factory
-    WriteEventFactory factory = new WriteEventFactory();
-
-    if (ringBufferSize == 0) {
-      ringBufferSize = 65536;
-    }
-
-    // ring buffer size always has to be the power of 2.
-    // so if it is not, make it equal to the nearest integer.
-    double power = Math.log(ringBufferSize) / Math.log(2);
-    if (power % 1 != 0) {
-      power = Math.ceil(power);
-      ringBufferSize = (int) Math.pow(2, power);
-      ProjectLogger.log("New ring buffer size = " + ringBufferSize);
-    }
-
-    // initialize our event handler.
-    WriteEventHandler handler = new WriteEventHandler();
-
-    // initialize the disruptor
-    disruptor = new Disruptor<Request>(factory, ringBufferSize, executor);
-    disruptor.handleEventsWith(handler);
-
-    // start the disruptor and get the generated ring buffer instance
-    disruptor.start();
-
-    // initialize the event producer to submit messages
-    writeEventProducer = new WriteEventProducer(disruptor);
   }
 
   /**
@@ -87,32 +43,7 @@ public class TelemetryLmaxWriter {
    * @param message telemetry request which contains telemetry event
    */
   public void submitMessage(Request message) {
-    if (writeEventProducer != null) {
-      // publish the messages via event producer
-      writeEventProducer.onData(message);
-    }
+
   }
 
-  /**
-   * Clean up thread to gracefully shutdown TelemetryLmaxWriter
-   *
-   * @author Manzarul
-   */
-  static class ResourceCleanUp extends Thread {
-    public void run() {
-      ProjectLogger.log("started resource cleanup.");
-      if (disruptor != null) {
-        disruptor.halt();
-        disruptor.shutdown();
-      }
-      ProjectLogger.log("completed resource cleanup.");
-    }
-  }
-
-  /** Register a shutdown hook to gracefully shutdown TelemetryLmaxWriter */
-  public static void registerShutDownHook() {
-    Runtime runtime = Runtime.getRuntime();
-    runtime.addShutdownHook(new ResourceCleanUp());
-    ProjectLogger.log("ShutDownHook registered.");
-  }
 }
