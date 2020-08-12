6081fc79787c69e933fc306fdc30300b8f69cba4
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2020-08-12 11:37:46 +0530

    SC-1852 Adding change to migration script

--- externalId-migration-util/src/main/java/org/sunbird/migration/ExternalIdMigrations.java
+++ externalId-migration-util/src/main/java/org/sunbird/migration/ExternalIdMigrations.java
@@ -1,51 +1,43 @@
 package org.sunbird.migration;
 
-import com.datastax.driver.core.Session;
 import org.apache.log4j.Logger;
 import org.sunbird.migration.connection.factory.CassandraConnectionFactory;
 import org.sunbird.migration.connection.factory.ConnectionFactory;
 import org.sunbird.migration.constants.EnvConstants;
-import org.sunbird.migration.connection.Connection;
-
 
 public class ExternalIdMigrations {
-    static Logger logger = LoggerFactory.getLoggerInstance(ExternalIdMigrations.class.getName());
-    private static String className = ExternalIdMigrations.class.getSimpleName();
+  static Logger logger = LoggerFactory.getLoggerInstance(ExternalIdMigrations.class.getName());
+  private static String className = ExternalIdMigrations.class.getSimpleName();
 
-    /**
-     * the Code begins here...
-     *
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-        RequestParams requestParams = prepareRequestParams();
-        RequestParamValidator.getInstance(requestParams).validate();
-        ConnectionFactory connectionFactory = new CassandraConnectionFactory();
-        RecordProcessor recordProcessor = RecordProcessor.getInstance(connectionFactory, requestParams);
-        generateRecords(recordProcessor);
-        recordProcessor.startProcessingExternalIds();
-    }
+  /**
+   * the Code begins here...
+   *
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    RequestParams requestParams = prepareRequestParams();
+    RequestParamValidator.getInstance(requestParams).validate();
+    ConnectionFactory connectionFactory = new CassandraConnectionFactory();
+    RecordProcessor recordProcessor = RecordProcessor.getInstance(connectionFactory, requestParams);
+    recordProcessor.startProcessingExternalIds();
+  }
 
-
-
-    /**
-     * this method will prepare RequestParams object while reading constants from env
-     *
-     * @return RequestParams
-     */
-    private static RequestParams prepareRequestParams() {
-        RequestParams requestParams = new RequestParams();
-        requestParams.setCassandraHost(System.getenv(EnvConstants.SUNBIRD_CASSANDRA_HOST));
-        requestParams.setCassandraKeyspaceName(
-                System.getenv(EnvConstants.SUNBIRD_CASSANDRA_KEYSPACENAME));
-        requestParams.setCassandraPort(System.getenv(EnvConstants.SUNBIRD_CASSANDRA_PORT));
-        logger.info(
-                String.format(
-                        "%s:%s:env variable got %s",
-                        className, "prepareRequestParams", requestParams.toString()));
-        return requestParams;
-    }
-
+  /**
+   * this method will prepare RequestParams object while reading constants from env
+   *
+   * @return RequestParams
+   */
+  private static RequestParams prepareRequestParams() {
+    RequestParams requestParams = new RequestParams();
+    requestParams.setCassandraHost(System.getenv(EnvConstants.SUNBIRD_CASSANDRA_HOST));
+    requestParams.setCassandraKeyspaceName(
+        System.getenv(EnvConstants.SUNBIRD_CASSANDRA_KEYSPACENAME));
+    requestParams.setCassandraPort(System.getenv(EnvConstants.SUNBIRD_CASSANDRA_PORT));
+    logger.info(
+        String.format(
+            "%s:%s:env variable got %s",
+            className, "prepareRequestParams", requestParams.toString()));
+    return requestParams;
+  }
 }
-
