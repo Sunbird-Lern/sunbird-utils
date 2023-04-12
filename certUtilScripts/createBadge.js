db8b1a5ce492d4dfbbbde7f7f05f91d53688fdc6
BharathwajShankar <saialumnibharathwaj@gmail.com>
2023-04-12 15:29:38 +0530

    Issue #LR-456 feat: Cleaning up the repo and updating the readme.md file

--- certUtilScripts/createBadge.js
+++
@@ -1,3 +0,0 @@
-var badge = require('./master.js');
-
-badge.createBadgeJson();
--- notification-sdk/src/main/java/org/sunbird/notification/sms/provider/ISmsProviderFactory.java
+++
@@ -1,8 +0,0 @@
-package org.sunbird.notification.sms.provider;
-
-import org.sunbird.notification.beans.SMSConfig;
-
-public interface ISmsProviderFactory {
-
-  ISmsProvider create(SMSConfig config);
-}
