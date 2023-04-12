6cce7ea344c17b8c14b270b918b519ff268a717c
Reshmi V Nair <54312456+reshmi-nair@users.noreply.github.com>
2023-04-12 12:46:35 +0530

    LR-521 Merging Release 5.2.0 to master (#1150)
    * Issue #LR-101 feat: Grouped the cql file based on keyspace (#1147)
    
    * Issue #LR-101 feat: Read the SCRIPT_LOCATIONS from config sunbird_cassandra_migration_location.
    
    * Issue #LR-101 feat: Removed the sunbirdplugin keyspace. Not used now.
    
    * Issue #LR-101 feat: added cassandra.cql file from sunbird-lms-service repo
    ---------
    
    Co-authored-by: Anil Gupta <ani2824@gmail.com>

--- sunbird-cassandra-migration/cassandra-migration/maven-central-deploy.sh
+++ sunbird-cassandra-migration/cassandra-migration/maven-central-deploy.sh
@@ -2,7 +2,7 @@
 # Deploy maven artefact in current directory into Maven central repository 
 # using maven-release-plugin goals
 
-read -p "Really deploy to maven cetral repository  (yes/no)? "
+read -p "Really deploy to maven central repository  (yes/no)? "
 
 if ( [ "$REPLY" == "yes" ] ) then
   mvn -P release release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
